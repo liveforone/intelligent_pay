de09ad28ce841c4f8f19133fc779902cba1c125f
liveforone <yc4852@gmail.com>
2023-05-09 19:12:22 +0900

    Add fallback message.

---
+++ gateway-service/gateway-service/src/main/java/intelligent_pay/gatewayservice/controller/FallbackMessage.java
@@ -0,0 +1,5 @@
+package intelligent_pay.gatewayservice.controller;
+
+public final class FallbackMessage {
+    public static final String USER_LOG = "유저 서비스의 장애로 접근이 불가능합니다.\n이용에 불편을 드려 죄송합니다.";
+}
