c28c7ac0904d58f78012716574c5c925388f7584
liveforone <yc4852@gmail.com>
2023-05-09 19:12:13 +0900

    Add fallback url constant class.

---
+++ gateway-service/gateway-service/src/main/java/intelligent_pay/gatewayservice/controller/FallbackUrl.java
@@ -0,0 +1,7 @@
+package intelligent_pay.gatewayservice.controller;
+
+public final class FallbackUrl {
+
+    public static final String BASE = "/fallback";
+    public static final String USER = "/user";
+}
