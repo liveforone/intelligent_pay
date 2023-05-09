d2d187beb5dc81ae756775c867c7c29b76e1401c
liveforone <yc4852@gmail.com>
2023-05-09 19:11:26 +0900

    Add global tracker log.

---
+++ gateway-service/gateway-service/src/main/java/intelligent_pay/gatewayservice/filter/constant/GlobalTrackerLog.java
@@ -0,0 +1,16 @@
+package intelligent_pay.gatewayservice.filter.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum GlobalTrackerLog {
+
+    BASE_LOG("Global filter base message: {}"),
+    START_LOG("Global filter start: request id -> {}"),
+    END_LOG("Global filter end: response code -> {}");
+
+    private final String value;
+
+}
