9abc1e85686827f87107a916ec1ed11ff19bf5ad
liveforone <yc4852@gmail.com>
2023-05-09 19:10:22 +0900

    Initial gateway-service.

---
+++ gateway-service/gateway-service/src/test/java/intelligent_pay/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package intelligent_pay.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
