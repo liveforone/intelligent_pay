114acefaed990e84912f2eefd13ef2640759adfc
liveforone <yc4852@gmail.com>
2023-05-14 21:49:55 +0900

    Add change member info request dto.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/dto/changeInfo/ChangeEmailRequest.java
@@ -0,0 +1,15 @@
+package intelligent_pay.userservice.dto.changeInfo;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChangeEmailRequest {
+
+    @Email(message = "이메일 형식을 지켜 작성해주세요.")
+    @NotBlank(message = "이메일은 반드시 기입해주세요.")
+    private String email;
+}
