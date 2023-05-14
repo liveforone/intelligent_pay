28f8400f5490bafec618143198c8601bbab24322
liveforone <yc4852@gmail.com>
2023-05-14 21:53:33 +0900

    Add member url and param constant.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/controller/constant/MemberParam.java
@@ -0,0 +1,7 @@
+package intelligent_pay.userservice.controller.constant;
+
+public final class MemberParam {
+    private MemberParam() {}
+
+    public static final String EMAIL = "email";
+}
---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/controller/constant/MemberUrl.java
@@ -0,0 +1,17 @@
+package intelligent_pay.userservice.controller.constant;
+
+public final class MemberUrl {
+
+    private MemberUrl() {}
+
+    public static final String SIGNUP = "/signup";
+    public static final String LOGIN = "/login";
+    public static final String MY_INFO = "/my-info";
+    public static final String CHANGE_EMAIL = "/change/email";
+    public static final String CHANGE_PASSWORD = "/change/password";
+    public static final String LOGOUT = "/logout";
+    public static final String WITHDRAW = "/withdraw";
+    public static final String ADMIN_SEARCH = "/admin/search";
+    public static final String PROHIBITION = "/prohibition";
+    public static final String ACTUATOR = "/actuator/**";
+}
