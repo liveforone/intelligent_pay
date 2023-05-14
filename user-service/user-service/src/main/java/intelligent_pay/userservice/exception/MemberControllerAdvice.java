ae153d35dba4a12a12e949b29433548d2c109226
liveforone <yc4852@gmail.com>
2023-05-14 21:52:16 +0900

    Add exception controller advice.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/exception/MemberControllerAdvice.java
@@ -0,0 +1,31 @@
+package intelligent_pay.userservice.exception;
+
+import intelligent_pay.userservice.controller.restResponse.RestResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MemberControllerAdvice {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    protected ResponseEntity<?> loginFailHandle() {
+        return RestResponse.loginFail();
+    }
+
+    @ExceptionHandler(MemberCustomException.class)
+    protected ResponseEntity<?> memberCustomHandle(MemberCustomException customException) {
+        return ResponseEntity
+                .status(customException.getResponseMessage().getStatus())
+                .body(customException.getResponseMessage().getValue());
+    }
+
+    @ExceptionHandler(BindingCustomException.class)
+    protected ResponseEntity<?> bindingErrorHandle(BindingCustomException customException) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(customException.getMessage());
+    }
+}
