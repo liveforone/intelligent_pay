c825327b8c843bd89353601258014e6ebf79ea83
liveforone <yc4852@gmail.com>
2023-05-14 21:53:00 +0900

    Add bankbook param and url constant.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/feignClient/constant/BankbookParam.java
@@ -0,0 +1,7 @@
+package intelligent_pay.userservice.feignClient.constant;
+
+public final class BankbookParam {
+    private BankbookParam() {}
+
+    public static final String USERNAME = "username";
+}
---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/feignClient/constant/BankbookUrl.java
@@ -0,0 +1,8 @@
+package intelligent_pay.userservice.feignClient.constant;
+
+public final class BankbookUrl {
+    private BankbookUrl() {}
+
+    public static final String BASE = "bankbook-service";
+    public static final String MY_BALANCE = "/balance/{username}";
+}
