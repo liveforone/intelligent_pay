e030a0f14d15bf82feade2fba7e0ac88cba8a741
liveforone <yc4852@gmail.com>
2023-05-14 19:10:00 +0900

    Initial user-service.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/UserServiceApplication.java
@@ -0,0 +1,29 @@
+package intelligent_pay.userservice;
+
+import feign.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLeve() {
+		return Logger.Level.FULL;
+	}
+}
