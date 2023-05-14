f39f6bd0b2f66bd4f6d7207af02835cb5600d381
liveforone <yc4852@gmail.com>
2023-05-14 21:48:52 +0900

    Add signup request dto.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/dto/signupAndLogin/MemberSignupRequest.java
@@ -0,0 +1,25 @@
+package intelligent_pay.userservice.dto.signupAndLogin;
+
+import intelligent_pay.userservice.domain.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MemberSignupRequest {
+
+    private Long id;
+
+    @Email(message = "이메일 형식을 지켜 작성해주세요.")
+    @NotBlank(message = "이메일은 반드시 기입해주세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 반드시 입력하세요.")
+    private String password;
+
+    @NotBlank(message = "실명은 반드시 기입해야하며, 잘못된 실명기입은 소비자가 책임집니다.")
+    private String realName;
+    private Role auth;
+}
