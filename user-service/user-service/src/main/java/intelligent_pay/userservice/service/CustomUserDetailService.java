c9bdcab17ae6b6556c4927d9d0ff9e513effd94a
liveforone <yc4852@gmail.com>
2023-05-14 21:52:03 +0900

    Add user detail service.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/service/CustomUserDetailService.java
@@ -0,0 +1,41 @@
+package intelligent_pay.userservice.service;
+
+import intelligent_pay.userservice.domain.Member;
+import intelligent_pay.userservice.domain.Role;
+import intelligent_pay.userservice.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return createUserDetails(memberRepository.findByUsername(email));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        if (member.getAuth() == Role.ADMIN) {
+            String ADMIN_ROLE = "ADMIN";
+            return User.builder()
+                    .username(member.getUsername())
+                    .password(member.getPassword())
+                    .roles(ADMIN_ROLE)
+                    .build();
+        } else {
+            String MEMBER_ROLE = "MEMBER";
+            return User.builder()
+                    .username(member.getUsername())
+                    .password(member.getPassword())
+                    .roles(MEMBER_ROLE)
+                    .build();
+        }
+    }
+}
