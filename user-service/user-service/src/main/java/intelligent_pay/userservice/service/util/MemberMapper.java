6469f3aae4aac1c352cdd33ffc3d6ac765e4c3e8
liveforone <yc4852@gmail.com>
2023-05-14 21:52:25 +0900

    Add member dto mapper.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/service/util/MemberMapper.java
@@ -0,0 +1,21 @@
+package intelligent_pay.userservice.service.util;
+
+import intelligent_pay.userservice.domain.Member;
+import intelligent_pay.userservice.dto.response.MemberResponse;
+import intelligent_pay.userservice.utility.CommonUtils;
+
+public class MemberMapper {
+
+    public static MemberResponse entityToDto(Member member) {
+        if (CommonUtils.isNull(member)) {
+            return new MemberResponse();
+        }
+
+        return MemberResponse.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .realName(member.getRealName())
+                .auth(member.getAuth())
+                .build();
+    }
+}
