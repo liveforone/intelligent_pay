cadff111dfb6536e7049cb0bfadb0bb71918ce7f
liveforone <yc4852@gmail.com>
2023-05-14 21:52:33 +0900

    Add member validator.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/validator/MemberValidator.java
@@ -0,0 +1,61 @@
+package intelligent_pay.userservice.validator;
+
+import intelligent_pay.userservice.controller.constant.ControllerLog;
+import intelligent_pay.userservice.controller.restResponse.ResponseMessage;
+import intelligent_pay.userservice.domain.Member;
+import intelligent_pay.userservice.domain.Role;
+import intelligent_pay.userservice.exception.BindingCustomException;
+import intelligent_pay.userservice.exception.MemberCustomException;
+import intelligent_pay.userservice.repository.MemberRepository;
+import intelligent_pay.userservice.utility.CommonUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class MemberValidator {
+
+    private final MemberRepository memberRepository;
+
+    static BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public void validatePassword(String inputPassword, String username) {
+        Member foundMember = memberRepository.findByUsername(username);
+        String originalPassword = foundMember.getPassword();
+
+        if (!passwordEncoder.matches(inputPassword, originalPassword)) {
+            throw new MemberCustomException(ResponseMessage.NOT_MATCH_PASSWORD);
+        }
+    }
+
+    public void validateBinding(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = Objects
+                    .requireNonNull(bindingResult.getFieldError())
+                    .getDefaultMessage();
+            throw new BindingCustomException(errorMessage);
+        }
+    }
+
+    public void validateDuplicateEmail(String email) {
+        Member member = memberRepository.findByEmail(email);
+
+        if (!CommonUtils.isNull(member)) {
+            throw new MemberCustomException(ResponseMessage.DUPLICATE_EMAIL);
+        }
+    }
+
+    public void validateAdmin(String username) {
+        Member member = memberRepository.findByUsername(username);
+        if (!member.getAuth().equals(Role.ADMIN)) {
+            log.error(ControllerLog.ADMIN_FAIL.getValue());
+            throw new MemberCustomException(ResponseMessage.PROHIBITION);
+        }
+    }
+}
