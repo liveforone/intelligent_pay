446ca925f6d518a45052a4ca3928546487c03a34
liveforone <yc4852@gmail.com>
2023-05-14 21:52:44 +0900

    Add circuit log.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/feignClient/constant/CircuitLog.java
@@ -0,0 +1,13 @@
+package intelligent_pay.userservice.feignClient.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CircuitLog {
+
+    USER_CIRCUIT_LOG("user-circuit-log");
+
+    private final String value;
+}
