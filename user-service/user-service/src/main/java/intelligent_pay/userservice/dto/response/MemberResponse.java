61355b02f6fdf9d3a57c2cf4260daf369db400f0
liveforone <yc4852@gmail.com>
2023-05-14 21:49:27 +0900

    Add member response dto.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/dto/response/MemberResponse.java
@@ -0,0 +1,19 @@
+package intelligent_pay.userservice.dto.response;
+
+import intelligent_pay.userservice.domain.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberResponse {
+
+    private Long id;
+    private String email;
+    private String realName;
+    private Role auth;
+}
