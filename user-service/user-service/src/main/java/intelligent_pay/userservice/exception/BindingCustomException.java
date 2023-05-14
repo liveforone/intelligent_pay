2fe0365d6141c384da8a64d0310604965b0a0d86
liveforone <yc4852@gmail.com>
2023-05-14 21:51:51 +0900

    Add binding custom exception.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/exception/BindingCustomException.java
@@ -0,0 +1,8 @@
+package intelligent_pay.userservice.exception;
+
+public class BindingCustomException extends RuntimeException{
+
+    public BindingCustomException(String message) {
+        super(message);
+    }
+}
