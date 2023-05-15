35a981dfb80f2b6c23d74f2d0e42553d8778dd03
liveforone <yc4852@gmail.com>
2023-05-15 21:14:26 +0900

    Initial bankbook-service.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/BankbookServiceApplication.java
@@ -0,0 +1,29 @@
+package intelligent_pay.bankbookservice;
+
+import feign.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class BankbookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankbookServiceApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLeve() {
+		return Logger.Level.FULL;
+	}
+}
---
+++ bankbook-service/bankbook-service/src/test/java/intelligent_pay/bankbookservice/BankbookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package intelligent_pay.bankbookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankbookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
