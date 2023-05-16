c3514f66932baa49f0181792326ebdd25ed585a2
liveforone <yc4852@gmail.com>
2023-05-16 19:48:17 +0900

    Add suspend bankbook response message.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/restResponse/ResponseMessage.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/restResponse/ResponseMessage.java
@@ -9,6 +9,7 @@ public enum ResponseMessage {
 
     BANKBOOK_IS_NULL(404, "계좌가 존재하지 않습니다."),
     DUPLICATE_BANKBOOK(400, "이미 계좌가 존재합니다."),
+    SUSPEND_BANKBOOK(400, "정지된 계좌입니다."),
     CREATE_BANKBOOK_SUCCESS(201, "계좌를 성공적으로 생성하였습니다."),
     PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다."),
     UPDATE_PASSWORD_SUCCESS(200, "비밀번호 변경에 성공하였습니다."),
