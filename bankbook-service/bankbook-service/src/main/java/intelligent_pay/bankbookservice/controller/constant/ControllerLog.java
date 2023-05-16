a248beab6941816e746fc1ac0dbd70cc79b3c588
liveforone <yc4852@gmail.com>
2023-05-16 19:38:44 +0900

    Add suspend and cancel suspend controller log.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/ControllerLog.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/ControllerLog.java
@@ -10,7 +10,9 @@ public enum ControllerLog {
     CREATE_BANKBOOK_SUCCESS("Create Bankbook Success"),
     ADD_BALANCE_SUCCESS("Add Balance Success | BankbookNum : "),
     SUBTRACT_BALANCE_SUCCESS("Subtract Balance Success | BankbookNum : "),
-    UPDATE_PASSWORD_SUCCESS("Update password Success | BankbookNum : ");
+    UPDATE_PASSWORD_SUCCESS("Update password Success | BankbookNum : "),
+    SUSPEND_SUCCESS("Suspend Bankbook Success | BankbookNum : "),
+    CANCEL_SUSPEND_SUCCESS("Suspend Bankbook Success | BankbookNum : ");
 
     private final String value;
 }
