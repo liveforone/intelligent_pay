b4b1309009cdbd0d51b71bfaa7161f93fa3d2915
liveforone <yc4852@gmail.com>
2023-05-16 14:02:59 +0900

    Add bankbook response dto.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/BankbookResponse.java
@@ -0,0 +1,22 @@
+package intelligent_pay.bankbookservice.dto;
+
+import intelligent_pay.bankbookservice.domain.BankbookState;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankbookResponse {
+
+    private Long id;
+    private long balance;
+    private String bankbookNum;
+    private BankbookState bankbookState;
+    private LocalDate createdDate;
+}
