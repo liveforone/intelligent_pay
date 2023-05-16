a4f460b18124117bb68e4929b08a2265e7b5ce22
liveforone <yc4852@gmail.com>
2023-05-16 14:10:40 +0900

    Add validate bankbook null validator.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/validator/BankbookValidator.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/validator/BankbookValidator.java
@@ -1,6 +1,10 @@
 package intelligent_pay.bankbookservice.validator;
 
+import intelligent_pay.bankbookservice.controller.restResponse.ResponseMessage;
+import intelligent_pay.bankbookservice.domain.Bankbook;
+import intelligent_pay.bankbookservice.exception.BankbookCustomException;
 import intelligent_pay.bankbookservice.repository.BankbookRepository;
+import intelligent_pay.bankbookservice.utility.CommonUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,5 +14,11 @@ public class BankbookValidator {
 
     private final BankbookRepository bankbookRepository;
 
+    public void validateBankbookNull(String username) {
+        Bankbook foundBankbook = bankbookRepository.findOneByUsername(username);
 
+        if (CommonUtils.isNull(foundBankbook)) {
+            throw new BankbookCustomException(ResponseMessage.BANKBOOK_IS_NULL);
+        }
+    }
 }
