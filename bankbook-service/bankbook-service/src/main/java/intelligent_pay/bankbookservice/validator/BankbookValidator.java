759a19216b3e117d81d20825118dfdfa89737a28
liveforone <yc4852@gmail.com>
2023-05-16 14:24:18 +0900

    Add validate binding validator.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/validator/BankbookValidator.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/validator/BankbookValidator.java
@@ -3,10 +3,14 @@ package intelligent_pay.bankbookservice.validator;
 import intelligent_pay.bankbookservice.controller.restResponse.ResponseMessage;
 import intelligent_pay.bankbookservice.domain.Bankbook;
 import intelligent_pay.bankbookservice.exception.BankbookCustomException;
+import intelligent_pay.bankbookservice.exception.BindingCustomException;
 import intelligent_pay.bankbookservice.repository.BankbookRepository;
 import intelligent_pay.bankbookservice.utility.CommonUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -19,6 +23,15 @@ public class BankbookValidator {
 
         if (CommonUtils.isNull(foundBankbook)) {
             throw new BankbookCustomException(ResponseMessage.BANKBOOK_IS_NULL);
+        }
+    }
+
+    public void validateBinding(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = Objects
+                    .requireNonNull(bindingResult.getFieldError())
+                    .getDefaultMessage();
+            throw new BindingCustomException(errorMessage);
         }
     }
 }
