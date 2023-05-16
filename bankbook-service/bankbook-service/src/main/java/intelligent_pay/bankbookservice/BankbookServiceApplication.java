8bd0e3464678bb3bdf3d2fc976956c39c864c4cc
liveforone <yc4852@gmail.com>
2023-05-16 11:52:08 +0900

    Add jpa auditing.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/BankbookServiceApplication.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/BankbookServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class BankbookServiceApplication {
 
 	public static void main(String[] args) {
