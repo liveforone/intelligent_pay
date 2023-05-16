e22136fcec0c7b02f17819b872b7217fe88ebe77
liveforone <yc4852@gmail.com>
2023-05-16 18:40:15 +0900

    Remove add balance response message.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/restResponse/ResponseMessage.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/restResponse/ResponseMessage.java
@@ -10,7 +10,6 @@ public enum ResponseMessage {
     BANKBOOK_IS_NULL(404, "계좌가 존재하지 않습니다."),
     DUPLICATE_BANKBOOK(400, "이미 계좌가 존재합니다."),
     CREATE_BANKBOOK_SUCCESS(201, "계좌를 성공적으로 생성하였습니다."),
-    ADD_BALANCE_SUCCESS(200, "입금에 성공하였습니다."),
     PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다.");
 
     private final int status;
