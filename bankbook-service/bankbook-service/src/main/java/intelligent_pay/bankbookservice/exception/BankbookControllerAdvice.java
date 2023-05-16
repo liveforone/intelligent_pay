46f96b186724332b98575fb59ae338515ecd7741
liveforone <yc4852@gmail.com>
2023-05-16 11:32:25 +0900

    Add bankbook controller advice for exception handling.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/exception/BankbookControllerAdvice.java
@@ -0,0 +1,24 @@
+package intelligent_pay.bankbookservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class BankbookControllerAdvice {
+
+    @ExceptionHandler(BankbookCustomException.class)
+    protected ResponseEntity<?> shopCustomHandle(BankbookCustomException customException) {
+        return ResponseEntity
+                .status(customException.getResponseMessage().getStatus())
+                .body(customException.getResponseMessage().getValue());
+    }
+
+    @ExceptionHandler(BindingCustomException.class)
+    protected ResponseEntity<?> bindingErrorHandle(BindingCustomException customException) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(customException.getMessage());
+    }
+}
