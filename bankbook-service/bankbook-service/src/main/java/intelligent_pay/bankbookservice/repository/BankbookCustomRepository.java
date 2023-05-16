3072035f18c374453964d0739a253c461cb9ab5c
liveforone <yc4852@gmail.com>
2023-05-16 13:46:41 +0900

    Add find one by username query method.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/repository/BankbookCustomRepository.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/repository/BankbookCustomRepository.java
@@ -1,4 +1,7 @@
 package intelligent_pay.bankbookservice.repository;
 
+import intelligent_pay.bankbookservice.domain.Bankbook;
+
 public interface BankbookCustomRepository {
+    Bankbook findOneByUsername(String username);
 }
--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/repository/BankbookRepositoryImpl.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/repository/BankbookRepositoryImpl.java
@@ -1,6 +1,8 @@
 package intelligent_pay.bankbookservice.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import intelligent_pay.bankbookservice.domain.Bankbook;
+import intelligent_pay.bankbookservice.domain.QBankbook;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,12 @@ import org.springframework.stereotype.Repository;
 public class BankbookRepositoryImpl implements BankbookCustomRepository{
 
     private final JPAQueryFactory queryFactory;
+    QBankbook bankbook = QBankbook.bankbook;
 
-
+    public Bankbook findOneByUsername(String username) {
+        return queryFactory
+                .selectFrom(bankbook)
+                .where(bankbook.username.eq(username))
+                .fetchOne();
+    }
 }
