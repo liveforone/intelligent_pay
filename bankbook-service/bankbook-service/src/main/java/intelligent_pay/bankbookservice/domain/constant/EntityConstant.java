520af81e4ef314884cbfe4a85ea0a801fc5dfe1f
liveforone <yc4852@gmail.com>
2023-05-16 13:38:12 +0900

    Add entity constant.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/domain/constant/EntityConstant.java
@@ -0,0 +1,8 @@
+package intelligent_pay.bankbookservice.domain.constant;
+
+public final class EntityConstant {
+    private EntityConstant() {}
+
+    public static final int INITIAL_BALANCE = 0;
+    public static final int SIZE_OF_BANKBOOK_NUM = 13;
+}
