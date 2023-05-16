2d7976cac9c395e7b7ed48aea5f435cab37faf7b
liveforone <yc4852@gmail.com>
2023-05-16 19:22:14 +0900

    Add update bankbook state request dto.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/UpdateBankbookStateRequest.java
@@ -0,0 +1,11 @@
+package intelligent_pay.bankbookservice.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UpdateBankbookStateRequest {
+
+    private String password;
+}
