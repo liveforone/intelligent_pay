d0c4f17bb605841fb5eefdec7993d320dd9a339c
liveforone <yc4852@gmail.com>
2023-05-16 13:20:58 +0900

    Add bankbook create domain logic.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/domain/Bankbook.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/domain/Bankbook.java
@@ -1,9 +1,12 @@
 package intelligent_pay.bankbookservice.domain;
 
+import intelligent_pay.bankbookservice.domain.util.PasswordUtil;
+import intelligent_pay.bankbookservice.dto.BankbookRequest;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -36,4 +39,17 @@ public class Bankbook {
     @CreatedDate
     @Column(updatable = false)
     private LocalDate createdDate;
+
+    private Bankbook(String password, String bankbookNum, String username) {
+        this.password = password;
+        this.bankbookNum = bankbookNum;
+        this.username = username;
+        this.bankbookState = BankbookState.WORK;
+    }
+
+    public static Bankbook create(BankbookRequest bankbookRequest, String username) {
+        String encodedPassword = PasswordUtil.encodePassword(bankbookRequest.getPassword());
+        String bankbookNum = RandomStringUtils.randomNumeric(13);
+        return new Bankbook(encodedPassword, bankbookNum, username);
+    }
 }
