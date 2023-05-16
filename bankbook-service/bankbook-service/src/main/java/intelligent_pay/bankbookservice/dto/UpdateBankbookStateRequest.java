dff6be67bea9fc2d7dd64018862c4c7671f84b31
liveforone <yc4852@gmail.com>
2023-05-16 19:22:52 +0900

    Add validation logic in dto.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/UpdateBankbookStateRequest.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/UpdateBankbookStateRequest.java
@@ -1,5 +1,7 @@
 package intelligent_pay.bankbookservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,5 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UpdateBankbookStateRequest {
 
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    @Size(min = 8, message = "비밀번호는 8자리 이상이어야 합니다.")
     private String password;
 }
