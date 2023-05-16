6e74d852be538b5d1308f9f62da7ff42e93292ed
liveforone <yc4852@gmail.com>
2023-05-16 15:09:19 +0900

    Add add balance request dto.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/AddBalanceRequest.java
@@ -0,0 +1,19 @@
+package intelligent_pay.bankbookservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class AddBalanceRequest {
+
+    @NotBlank(message = "계좌번호를 입력해주세요.")
+    @Size(min = 13, message = "계좌번호 양식이 올바르지 않습니다.")
+    private String bankbookNum;
+
+    @Positive(message = "입금금액은 양수만 가능합니다.")
+    private long money;
+}
