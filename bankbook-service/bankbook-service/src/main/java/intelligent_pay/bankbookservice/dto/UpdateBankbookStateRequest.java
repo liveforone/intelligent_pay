7651cd85bdb0e423b932654f34443f74cd084608
liveforone <yc4852@gmail.com>
2023-05-16 19:29:45 +0900

    Add bankbook num in dot.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/UpdateBankbookStateRequest.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/UpdateBankbookStateRequest.java
@@ -12,4 +12,7 @@ public class UpdateBankbookStateRequest {
     @NotBlank(message = "비밀번호를 입력하세요.")
     @Size(min = 8, message = "비밀번호는 8자리 이상이어야 합니다.")
     private String password;
+
+    @NotBlank(message = "계좌번호를 입력하세요.")
+    private String bankbookNum;
 }
