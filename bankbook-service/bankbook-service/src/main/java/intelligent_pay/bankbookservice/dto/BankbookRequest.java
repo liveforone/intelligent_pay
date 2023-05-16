fc3f27531adfa4c77804eeccb148a0a513d01290
liveforone <yc4852@gmail.com>
2023-05-16 13:14:10 +0900

    Add bankbook request dto.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/BankbookRequest.java
@@ -0,0 +1,15 @@
+package intelligent_pay.bankbookservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class BankbookRequest {
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    @Size(min = 8, message = "비밀번호는 8자리 이상이어야 합니다.")
+    private String password;
+}
