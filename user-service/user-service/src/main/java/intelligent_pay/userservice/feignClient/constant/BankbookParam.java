7f43f46e269069ce6c7bc283dec7589da7d4adf6
liveforone <yc4852@gmail.com>
2023-05-16 13:32:19 +0900

    Remove bankbook param.

--- user-service/user-service/src/main/java/intelligent_pay/userservice/feignClient/constant/BankbookParam.java
+++
@@ -1,7 +0,0 @@
-package intelligent_pay.userservice.feignClient.constant;
-
-public final class BankbookParam {
-    private BankbookParam() {}
-
-    public static final String USERNAME = "username";
-}
