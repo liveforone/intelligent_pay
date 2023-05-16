a6500212375114a80beb6cc87afeaa9b6237e556
liveforone <yc4852@gmail.com>
2023-05-16 17:48:41 +0900

    Add bankbook Custom Bool exception.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/exception/returnBool/BankbookCustomBoolException.java
@@ -0,0 +1,7 @@
+package intelligent_pay.bankbookservice.exception.returnBool;
+
+public class BankbookCustomBoolException extends RuntimeException {
+    public BankbookCustomBoolException() {
+        super();
+    }
+}
