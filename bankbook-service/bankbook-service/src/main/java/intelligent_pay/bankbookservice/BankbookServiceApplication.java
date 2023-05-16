1c2662018974936f968374b302060621f470555d
liveforone <yc4852@gmail.com>
2023-05-16 11:36:13 +0900

    Remove feign.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/BankbookServiceApplication.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/BankbookServiceApplication.java
@@ -1,16 +1,13 @@
 package intelligent_pay.bankbookservice;
 
-import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class BankbookServiceApplication {
 
 	public static void main(String[] args) {
@@ -20,10 +17,5 @@ public class BankbookServiceApplication {
 	@Bean
 	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
 		return new HiddenHttpMethodFilter();
-	}
-
-	@Bean
-	public Logger.Level feignLoggerLeve() {
-		return Logger.Level.FULL;
 	}
 }
