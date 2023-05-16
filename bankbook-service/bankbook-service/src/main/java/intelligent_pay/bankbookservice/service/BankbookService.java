3f16985a4ec4703226aada673a60f17c4e7284d2
liveforone <yc4852@gmail.com>
2023-05-16 14:04:13 +0900

    Add get bankbook by username service method.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/service/BankbookService.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/service/BankbookService.java
@@ -1,6 +1,8 @@
 package intelligent_pay.bankbookservice.service;
 
+import intelligent_pay.bankbookservice.dto.BankbookResponse;
 import intelligent_pay.bankbookservice.repository.BankbookRepository;
+import intelligent_pay.bankbookservice.service.util.BankbookMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,5 +14,9 @@ public class BankbookService {
 
     private final BankbookRepository bankbookRepository;
 
-
+    public BankbookResponse getBankbookByUsername(String username) {
+        return BankbookMapper.entityToDto(
+                bankbookRepository.findOneByUsername(username)
+        );
+    }
 }
