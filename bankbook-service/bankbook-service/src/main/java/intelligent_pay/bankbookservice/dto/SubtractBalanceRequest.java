03d0c6f16ebe2d2927ebe12bf927be2d755857e5
liveforone <yc4852@gmail.com>
2023-05-16 17:31:02 +0900

    Add subtract balance request dto.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/SubtractBalanceRequest.java
@@ -0,0 +1,25 @@
+package intelligent_pay.bankbookservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@NotBlank
+public class SubtractBalanceRequest {
+
+    @NotBlank(message = "계좌번호를 입력해주세요.")
+    @Size(min = 13, message = "계좌번호 양식이 올바르지 않습니다.")
+    private String bankbookNum;
+
+    @Positive(message = "입금금액은 양수만 가능합니다.")
+    private long money;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
+
+    @NotBlank(message = "계좌번호를 입력해주세요.")
+    @Size(min = 13, message = "계좌번호 양식이 올바르지 않습니다.")
+    private String otherBankbookNum;
+}
