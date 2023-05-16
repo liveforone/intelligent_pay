99540ae805dcefc4fc97e94ef5704d593cd09a1f
liveforone <yc4852@gmail.com>
2023-05-16 14:20:18 +0900

    Add info controller method.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/BankbookController.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/BankbookController.java
@@ -1,9 +1,9 @@
 package intelligent_pay.bankbookservice.controller;
 
 import intelligent_pay.bankbookservice.authentication.AuthenticationInfo;
-import intelligent_pay.bankbookservice.controller.constant.BankbookUrl;
 import intelligent_pay.bankbookservice.dto.BankbookResponse;
 import intelligent_pay.bankbookservice.service.BankbookService;
+import intelligent_pay.bankbookservice.utility.CommonUtils;
 import intelligent_pay.bankbookservice.validator.BankbookValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static intelligent_pay.bankbookservice.controller.constant.BankbookUrl.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,12 +23,20 @@ public class BankbookController {
     private final AuthenticationInfo authenticationInfo;
     private final BankbookValidator bankbookValidator;
 
-    @GetMapping(BankbookUrl.RETURN_BALANCE)
+    @GetMapping(RETURN_BALANCE)
     public ResponseEntity<?> returnBalance(HttpServletRequest request) {
+        String username = authenticationInfo.getUsername(request);
+
+        BankbookResponse bankbook = bankbookService.getBankbookByUsername(username);
+        return ResponseEntity.ok(CommonUtils.isNull(bankbook) ? 0 : bankbook.getBalance());
+    }
+
+    @GetMapping(INFO)
+    public ResponseEntity<?> bankbookInfo(HttpServletRequest request) {
         String username = authenticationInfo.getUsername(request);
         bankbookValidator.validateBankbookNull(username);
 
         BankbookResponse bankbook = bankbookService.getBankbookByUsername(username);
-        return ResponseEntity.ok(bankbook.getBalance());
+        return ResponseEntity.ok(bankbook);
     }
 }
