5e1b02a2133e33bd506e56c6b37f93809707ffb9
liveforone <yc4852@gmail.com>
2023-05-16 14:07:07 +0900

    Add bankbook url. And add return balance api.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/BankbookUrl.java
@@ -0,0 +1,7 @@
+package intelligent_pay.bankbookservice.controller.constant;
+
+public final class BankbookUrl {
+    private BankbookUrl() {}
+
+    public static final String RETURN_BALANCE = "/balance";
+}
