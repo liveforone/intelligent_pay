03536ed1eb0d6031bd95d72feaf1214c1bb1fd8c
liveforone <yc4852@gmail.com>
2023-05-16 17:43:58 +0900

    Add binding custom exception bool.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/exception/returnBool/BindingCustomBoolException.java
@@ -0,0 +1,7 @@
+package intelligent_pay.bankbookservice.exception.returnBool;
+
+public class BindingCustomBoolException extends RuntimeException {
+    public BindingCustomBoolException(String message) {
+        super(message);
+    }
+}
