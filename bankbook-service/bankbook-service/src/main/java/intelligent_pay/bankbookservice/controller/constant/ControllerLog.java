c83d1f1847c72f2c8382d6ece61677ecf1635ea4
liveforone <yc4852@gmail.com>
2023-05-16 14:57:57 +0900

    Add controller log and add create bankbook success log.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/ControllerLog.java
@@ -0,0 +1,13 @@
+package intelligent_pay.bankbookservice.controller.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ControllerLog {
+
+    CREATE_BANKBOOK_SUCCESS("Create Bankbook Success");
+
+    private final String value;
+}
