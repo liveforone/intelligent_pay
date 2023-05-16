20496dc2ab329129de475245f5dd4c71f03b76ea
liveforone <yc4852@gmail.com>
2023-05-16 14:05:16 +0900

    Add null check in mapper.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/service/util/BankbookMapper.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/service/util/BankbookMapper.java
@@ -2,10 +2,14 @@ package intelligent_pay.bankbookservice.service.util;
 
 import intelligent_pay.bankbookservice.domain.Bankbook;
 import intelligent_pay.bankbookservice.dto.BankbookResponse;
+import intelligent_pay.bankbookservice.utility.CommonUtils;
 
 public class BankbookMapper {
 
     public static BankbookResponse entityToDto(Bankbook bankbook) {
+        if (CommonUtils.isNull(bankbook)) {
+            return new BankbookResponse();
+        }
         return BankbookResponse.builder()
                 .id(bankbook.getId())
                 .balance(bankbook.getBalance())
