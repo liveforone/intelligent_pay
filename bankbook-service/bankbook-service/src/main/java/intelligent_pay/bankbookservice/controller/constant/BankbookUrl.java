4e2f817f4e612205a0cac07dcc5c72c06aa8ade0
liveforone <yc4852@gmail.com>
2023-05-16 19:20:28 +0900

    Add suspend url.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/BankbookUrl.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/BankbookUrl.java
@@ -9,4 +9,5 @@ public final class BankbookUrl {
     public static final String ADD_BALANCE = "/add/balance";
     public static final String SUBTRACT_BALANCE = "/subtract/balance";
     public static final String UPDATE_PASSWORD = "/update/password";
+    public static final String SUSPEND = "/suspend";
 }
