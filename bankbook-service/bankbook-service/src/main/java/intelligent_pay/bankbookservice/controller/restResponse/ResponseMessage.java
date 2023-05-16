60aed1b2be32692c071f0c832ce8ea07c4c5f74a
liveforone <yc4852@gmail.com>
2023-05-16 14:46:09 +0900

    Add duplicate bankbook response message.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/restResponse/ResponseMessage.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/restResponse/ResponseMessage.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum ResponseMessage {
 
     BANKBOOK_IS_NULL(404, "계좌가 존재하지 않습니다."),
+    DUPLICATE_BANKBOOK(400, "이미 계좌가 존재합니다."),
     CREATE_BANKBOOK_SUCCESS(201, "계좌를 성공적으로 생성하였습니다.");
 
     private final int status;
