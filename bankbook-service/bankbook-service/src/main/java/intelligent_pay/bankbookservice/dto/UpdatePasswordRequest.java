98244e408af4d433d091150222b7461b37818449
liveforone <yc4852@gmail.com>
2023-05-16 18:55:03 +0900

    Add validation annotation in request dto.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/UpdatePasswordRequest.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/UpdatePasswordRequest.java
@@ -1,5 +1,7 @@
 package intelligent_pay.bankbookservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +9,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UpdatePasswordRequest {
 
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    @Size(min = 8, message = "비밀번호는 8자리 이상이어야 합니다.")
     private String password;
+
+    @NotBlank(message = "새 비밀번호를 입력하세요.")
+    @Size(min = 8, message = "비밀번호는 8자리 이상이어야 합니다.")
     private String newPassword;
 }
