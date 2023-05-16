8b4287ace861d2e05bd2f95ca76528bc144d55c0
liveforone <yc4852@gmail.com>
2023-05-16 21:41:09 +0900

    Add create bankbook test.

--- bankbook-service/bankbook-service/src/test/java/intelligent_pay/bankbookservice/service/BankbookServiceTest.java
+++ bankbook-service/bankbook-service/src/test/java/intelligent_pay/bankbookservice/service/BankbookServiceTest.java
@@ -1,11 +1,14 @@
 package intelligent_pay.bankbookservice.service;
 
+import intelligent_pay.bankbookservice.domain.BankbookState;
+import intelligent_pay.bankbookservice.dto.BankbookRequest;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class BankbookServiceTest {
@@ -17,7 +20,22 @@ class BankbookServiceTest {
     EntityManager em;
 
     @Test
-    void createBankbook() {
+    @Transactional
+    void createBankbookTest() {
+        //given
+        String password = "1111111111111111";
+        String username = "onofwnofecaoeflefnaxxxxefeaowfewqfefa";
+        BankbookRequest request = new BankbookRequest();
+        request.setPassword(password);
+
+        //when
+        bankbookService.createBankbook(request, username);
+        em.flush();
+        em.clear();
+
+        //then
+        assertThat(bankbookService.getBankbookByUsername(username).getBankbookState())
+                .isEqualTo(BankbookState.WORK);
     }
 
     @Test
