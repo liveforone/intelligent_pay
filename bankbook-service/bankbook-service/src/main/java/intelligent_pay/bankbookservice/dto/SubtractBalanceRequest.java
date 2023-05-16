5e5a83cc47f704d2d72bafa7d622f1b59ebe63ee
liveforone <yc4852@gmail.com>
2023-05-16 18:43:04 +0900

    Refactor subtrac balance request remove otherBankbookNum.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/SubtractBalanceRequest.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/dto/SubtractBalanceRequest.java
@@ -18,8 +18,4 @@ public class SubtractBalanceRequest {
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
-
-    @NotBlank(message = "계좌번호를 입력해주세요.")
-    @Size(min = 13, message = "계좌번호 양식이 올바르지 않습니다.")
-    private String otherBankbookNum;
 }
