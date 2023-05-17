5831eb64e15d6929e6755b7f7ef9d70d72021618
liveforone <yc4852@gmail.com>
2023-05-17 22:55:30 +0900

    Add member domain test.

---
+++ user-service/user-service/src/test/java/intelligent_pay/userservice/domain/MemberDomainTest.java
@@ -0,0 +1,21 @@
+package intelligent_pay.userservice.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberDomainTest {
+
+    @Test
+    void createAdminTest() {
+
+    }
+
+    @Test
+    void updateEmail() {
+    }
+
+    @Test
+    void updatePassword() {
+    }
+}
