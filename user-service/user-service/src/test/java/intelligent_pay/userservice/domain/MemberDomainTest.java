6dc81e5b2dfb641424a7f5d664b9b57c46705737
liveforone <yc4852@gmail.com>
2023-05-17 22:59:37 +0900

    Add create admin domain test.

--- user-service/user-service/src/test/java/intelligent_pay/userservice/domain/MemberDomainTest.java
+++ user-service/user-service/src/test/java/intelligent_pay/userservice/domain/MemberDomainTest.java
@@ -1,14 +1,30 @@
 package intelligent_pay.userservice.domain;
 
+import intelligent_pay.userservice.dto.signupAndLogin.MemberSignupRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class MemberDomainTest {
 
     @Test
     void createAdminTest() {
+        //given
+        String email = "admin@intelligentpay.com";
+        String realName = "admin";
+        String password = "111111111111";
+        MemberSignupRequest request = new MemberSignupRequest();
+        request.setEmail(email);
+        request.setRealName(realName);
+        request.setPassword(password);
 
+        //when
+        Member member = Member.create(request);
+
+        //then
+        assertThat(member.getAuth())
+                .isEqualTo(Role.ADMIN);
     }
 
     @Test
