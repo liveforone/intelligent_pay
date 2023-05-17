459d003c00da44e8c1ac924660cf442f65e52b21
liveforone <yc4852@gmail.com>
2023-05-17 21:03:00 +0900

    Add username param.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/BankbookParam.java
@@ -0,0 +1,7 @@
+package intelligent_pay.bankbookservice.controller.constant;
+
+public final class BankbookParam {
+    private BankbookParam() {}
+
+    public static final String USERNAME = "username";
+}
