878d180b25f1504c2d9f8a0823226a9bb5a1a4d8
liveforone <yc4852@gmail.com>
2023-05-17 20:59:40 +0900

    Add path in balance url.

--- user-service/user-service/src/main/java/intelligent_pay/userservice/feignClient/constant/BankbookUrl.java
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/feignClient/constant/BankbookUrl.java
@@ -4,5 +4,5 @@ public final class BankbookUrl {
     private BankbookUrl() {}
 
     public static final String BASE = "bankbook-service";
-    public static final String MY_BALANCE = "/balance";
+    public static final String MY_BALANCE = "/balance/{username}";
 }
