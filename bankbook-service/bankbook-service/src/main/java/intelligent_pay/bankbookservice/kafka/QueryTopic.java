a404e486b1d975db29e4d6d507685581e3c200e5
liveforone <yc4852@gmail.com>
2023-05-18 18:44:16 +0900

    Add kafka query topic.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/kafka/QueryTopic.java
@@ -0,0 +1,8 @@
+package intelligent_pay.bankbookservice.kafka;
+
+public final class QueryTopic {
+
+    private QueryTopic() {}
+
+    public static final String REMOVE_BANKBOOK_BELONG_USER = "remove-bankbook-belong-user";
+}
