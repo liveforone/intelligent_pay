c939b718d96a4449b9d6985e629ce45950c71d74
liveforone <yc4852@gmail.com>
2023-05-18 22:03:14 +0900

    Add find records by bankbookNum paging.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/repository/RecordCustomRepository.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/repository/RecordCustomRepository.java
@@ -1,9 +1,13 @@
 package intelligent_pay.recordservice.repository;
 
 import intelligent_pay.recordservice.domain.Record;
+import intelligent_pay.recordservice.dto.RecordResponse;
+
+import java.util.List;
 
 public interface RecordCustomRepository {
 
     Record findOneByUsername(String username);
     Record findOneById(Long id);
+    List<RecordResponse> findRecordsByBankbookNum(String bankbookNum, Long lastId);
 }
--- record-service/record-service/src/main/java/intelligent_pay/recordservice/repository/RecordRepositoryImpl.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/repository/RecordRepositoryImpl.java
@@ -3,8 +3,14 @@ package intelligent_pay.recordservice.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import intelligent_pay.recordservice.domain.QRecord;
 import intelligent_pay.recordservice.domain.Record;
+import intelligent_pay.recordservice.dto.RecordResponse;
+import intelligent_pay.recordservice.repository.util.RecordRepositoryUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static intelligent_pay.recordservice.repository.util.RecordRepositoryUtil.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -25,5 +31,21 @@ public class RecordRepositoryImpl implements RecordCustomRepository {
                 .selectFrom(record)
                 .where(record.id.eq(id))
                 .fetchOne();
+    }
+
+    public List<RecordResponse> findRecordsByBankbookNum(
+            String bankbookNum,
+            Long lastId
+    ) {
+        return queryFactory
+                .select(dtoConstructor())
+                .from(record)
+                .where(
+                        record.bankBookNum.eq(bankbookNum),
+                        ltRecordId(lastId)
+                )
+                .orderBy(record.id.desc())
+                .limit(PAGE_SIZE)
+                .fetch();
     }
 }
