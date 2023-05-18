48491530fda6cfaca2b9e3fa4ab7432d09a8c220
liveforone <yc4852@gmail.com>
2023-05-18 21:59:07 +0900

    Add record repository util.

---
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/repository/util/RecordRepositoryUtil.java
@@ -0,0 +1,34 @@
+package intelligent_pay.recordservice.repository.util;
+
+import com.querydsl.core.types.ConstructorExpression;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import intelligent_pay.recordservice.domain.QRecord;
+import intelligent_pay.recordservice.dto.RecordResponse;
+
+public class RecordRepositoryUtil {
+
+    private static final QRecord record;
+
+    static {
+        record = QRecord.record;
+    }
+
+    public static BooleanExpression ltRecordId(Long lastId) {
+        if (lastId == 0) {
+            return null;
+        }
+
+        return record.id.lt(lastId);
+    }
+
+    public static ConstructorExpression<RecordResponse> dtoConstructor() {
+        return Projections.constructor(RecordResponse.class,
+                record.id,
+                record.title,
+                record.money,
+                record.recordState,
+                record.createdYear,
+                record.createdMonth);
+    }
+}
