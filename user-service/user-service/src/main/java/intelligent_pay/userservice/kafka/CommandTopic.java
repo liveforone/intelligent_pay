5b682edb03665e8637e517526413dfe400932785
liveforone <yc4852@gmail.com>
2023-05-18 18:29:58 +0900

    Refactor rename topic to command topic.

--- user-service/user-service/src/main/java/intelligent_pay/userservice/kafka/CommandTopic.java
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/kafka/CommandTopic.java
@@ -1,8 +1,8 @@
 package intelligent_pay.userservice.kafka;
 
-public final class Topic {
+public final class CommandTopic {
 
-    private Topic() {}
+    private CommandTopic() {}
 
     public static final String REMOVE_BANKBOOK = "remove-bankbook-belong-user";
     public static final String REMOVE_RECORD = "remove-record-belong-user";
