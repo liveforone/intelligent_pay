b9375158f96308ce23a863041b379a24aa1ce373
liveforone <yc4852@gmail.com>
2023-05-18 21:39:45 +0900

    Add find one by username query method.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/repository/RecordCustomRepository.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/repository/RecordCustomRepository.java
@@ -1,4 +1,8 @@
 package intelligent_pay.recordservice.repository;
 
+import intelligent_pay.recordservice.domain.Record;
+
 public interface RecordCustomRepository {
+
+    Record findOneByUsername(String username);
 }
--- record-service/record-service/src/main/java/intelligent_pay/recordservice/repository/RecordRepositoryImpl.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/repository/RecordRepositoryImpl.java
@@ -1,6 +1,8 @@
 package intelligent_pay.recordservice.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import intelligent_pay.recordservice.domain.QRecord;
+import intelligent_pay.recordservice.domain.Record;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,12 @@ import org.springframework.stereotype.Repository;
 public class RecordRepositoryImpl implements RecordCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+    QRecord record = QRecord.record;
+
+    public Record findOneByUsername(String username) {
+        return queryFactory
+                .selectFrom(record)
+                .where(record.username.eq(username))
+                .fetchOne();
+    }
 }
