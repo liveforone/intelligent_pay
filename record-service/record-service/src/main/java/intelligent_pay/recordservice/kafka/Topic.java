87e035ec8e15b5ea08550479c3acdec3940ace67
liveforone <yc4852@gmail.com>
2023-05-18 20:23:44 +0900

    Add topic.

---
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/kafka/Topic.java
@@ -0,0 +1,7 @@
+package intelligent_pay.recordservice.kafka;
+
+public final class Topic {
+
+    private Topic() {}
+    public static final String REMOVE_RECORD_BELONG_USER = "remove-record-belong-user";
+}
