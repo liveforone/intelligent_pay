17f4cc66c74d521e4e9a0f0f787291c8321d2395
liveforone <yc4852@gmail.com>
2023-05-18 18:29:23 +0900

    Add kafka topic.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/kafka/Topic.java
@@ -0,0 +1,9 @@
+package intelligent_pay.userservice.kafka;
+
+public final class Topic {
+
+    private Topic() {}
+
+    public static final String REMOVE_BANKBOOK = "remove-bankbook-belong-user";
+    public static final String REMOVE_RECORD = "remove-record-belong-user";
+}
