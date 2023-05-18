e64bf93e8456c5b034552f068b8f5c4a29d7d5d2
liveforone <yc4852@gmail.com>
2023-05-18 13:54:39 +0900

    Add bankbook query service.

---
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/query/BankbookQueryService.java
@@ -0,0 +1,27 @@
+package intelligent_pay.bankbookservice.query;
+
+import intelligent_pay.bankbookservice.dto.response.BankbookResponse;
+import intelligent_pay.bankbookservice.dto.response.BasicInfoResponse;
+import intelligent_pay.bankbookservice.repository.BankbookRepository;
+import intelligent_pay.bankbookservice.query.util.BankbookMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BankbookQueryService {
+
+    private final BankbookRepository bankbookRepository;
+
+    public BankbookResponse getBankbookByUsername(String username) {
+        return BankbookMapper.entityToDto(
+                bankbookRepository.findOneByUsername(username)
+        );
+    }
+
+    public BasicInfoResponse getBasicInfoByUsername(String username) {
+        return bankbookRepository.findBasicInfoByUsername(username);
+    }
+}
