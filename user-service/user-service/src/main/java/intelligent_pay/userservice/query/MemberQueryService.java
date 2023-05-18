f73e8f7f767606fcaa0f19f38a27bf0fcfa596c1
liveforone <yc4852@gmail.com>
2023-05-18 13:42:39 +0900

    Add member query service.

---
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/query/MemberQueryService.java
@@ -0,0 +1,26 @@
+package intelligent_pay.userservice.query;
+
+import intelligent_pay.userservice.dto.response.MemberResponse;
+import intelligent_pay.userservice.repository.MemberRepository;
+import intelligent_pay.userservice.service.util.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberQueryService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberResponse getMemberByUsername(String username) {
+        return MemberMapper.entityToDto(memberRepository.findByUsername(username));
+    }
+
+    public List<MemberResponse> searchByEmail(String email) {
+        return memberRepository.searchMemberByEmail(email);
+    }
+}
