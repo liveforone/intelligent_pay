804be4ae6feb5fcbfd39bfc856dfe1d02dd3dfd8
liveforone <yc4852@gmail.com>
2023-05-18 21:53:53 +0900

    Add validate binding validator.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/validator/RecordValidator.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/validator/RecordValidator.java
@@ -1,12 +1,25 @@
 package intelligent_pay.recordservice.validator;
 
+import intelligent_pay.recordservice.exception.BindingCustomException;
 import intelligent_pay.recordservice.repository.RecordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 public class RecordValidator {
 
     private final RecordRepository recordRepository;
+
+    public void validateBinding(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = Objects
+                    .requireNonNull(bindingResult.getFieldError())
+                    .getDefaultMessage();
+            throw new BindingCustomException(errorMessage);
+        }
+    }
 }
