6ae98b6c7cc93ea42657f07f7d433c38473f4786
liveforone <yc4852@gmail.com>
2023-05-18 21:32:32 +0900

    Add record mapper, and entity to dto mapper method.

---
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/query/util/RecordMapper.java
@@ -0,0 +1,18 @@
+package intelligent_pay.recordservice.query.util;
+
+import intelligent_pay.recordservice.domain.Record;
+import intelligent_pay.recordservice.dto.RecordResponse;
+
+public class RecordMapper {
+
+    public static RecordResponse entityToDto(Record record) {
+        return RecordResponse.builder()
+                .id(record.getId())
+                .title(record.getTitle())
+                .money(record.getMoney())
+                .recordState(record.getRecordState())
+                .createdYear(record.getCreatedYear())
+                .createdMonth(record.getCreatedMonth())
+                .build();
+    }
+}
