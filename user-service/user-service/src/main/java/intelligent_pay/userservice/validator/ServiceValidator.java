b7219e919f89686eea56644b840d3caa7477c17e
liveforone <yc4852@gmail.com>
2023-05-19 23:39:43 +0900

    Add validate password and duplicate email validator.

--- user-service/user-service/src/main/java/intelligent_pay/userservice/validator/ServiceValidator.java
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/validator/ServiceValidator.java
@@ -6,6 +6,7 @@ import intelligent_pay.userservice.exception.MemberCustomException;
 import intelligent_pay.userservice.repository.MemberRepository;
 import intelligent_pay.userservice.utility.CommonUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,11 +15,22 @@ public class ServiceValidator {
 
     private final MemberRepository memberRepository;
 
+    static BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public void validateDuplicateEmail(String email) {
         Member member = memberRepository.findByEmail(email);
 
         if (!CommonUtils.isNull(member)) {
             throw new MemberCustomException(ResponseMessage.DUPLICATE_EMAIL);
+        }
+    }
+
+    public void validatePassword(String inputPassword, String username) {
+        Member foundMember = memberRepository.findByUsername(username);
+        String originalPassword = foundMember.getPassword();
+
+        if (!passwordEncoder.matches(inputPassword, originalPassword)) {
+            throw new MemberCustomException(ResponseMessage.NOT_MATCH_PASSWORD);
         }
     }
 }
