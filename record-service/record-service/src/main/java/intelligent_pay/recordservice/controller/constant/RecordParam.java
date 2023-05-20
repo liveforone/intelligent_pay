ca59eb479e8d8dd14d19ce3d396b243da8b59ddc
liveforone <yc4852@gmail.com>
2023-05-20 16:17:51 +0900

    Add default year.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/controller/constant/RecordParam.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/controller/constant/RecordParam.java
@@ -1,5 +1,7 @@
 package intelligent_pay.recordservice.controller.constant;
 
+import java.time.LocalDate;
+
 public final class RecordParam {
     private RecordParam() {}
 
@@ -8,4 +10,5 @@ public final class RecordParam {
     public static final String LAST_ID = "lastId";
     public static final String DEFAULT_ID = "0";
     public static final String YEAR = "year";
+    public static final String DEFAULT_YEAR = String.valueOf(LocalDate.now().getYear());
 }
