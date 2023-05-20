c3d92c4623750b1d58e2878f2c51214f28e664ed
liveforone <yc4852@gmail.com>
2023-05-20 16:04:22 +0900

    Add last id and default id.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/controller/constant/RecordParam.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/controller/constant/RecordParam.java
@@ -4,4 +4,7 @@ public final class RecordParam {
     private RecordParam() {}
 
     public static final String ID = "id";
+    public static final String BANKBOOK_NUM = "bankbookNum";
+    public static final String LAST_ID = "lastId";
+    public static final String DEFAULT_ID = "0";
 }
