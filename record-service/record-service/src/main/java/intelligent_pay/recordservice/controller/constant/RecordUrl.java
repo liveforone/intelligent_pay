743fa005cbccc33d8257f629b6182a8139e970eb
liveforone <yc4852@gmail.com>
2023-05-20 15:57:00 +0900

    Add record detail url.

---
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/controller/constant/RecordUrl.java
@@ -0,0 +1,7 @@
+package intelligent_pay.recordservice.controller.constant;
+
+public final class RecordUrl {
+    private RecordUrl() {}
+
+    public static final String RECORD_DETAIL = "/record/info/{id}";
+}
