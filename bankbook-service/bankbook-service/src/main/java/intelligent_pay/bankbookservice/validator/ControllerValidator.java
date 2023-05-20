e78b1d059d83a5e86454e4ea62f17dc94da7aa17
liveforone <yc4852@gmail.com>
2023-05-20 15:24:49 +0900

    Add validate username validator.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/validator/ControllerValidator.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/validator/ControllerValidator.java
@@ -1,14 +1,22 @@
 package intelligent_pay.bankbookservice.validator;
 
+import intelligent_pay.bankbookservice.controller.restResponse.ResponseMessage;
+import intelligent_pay.bankbookservice.exception.BankbookCustomException;
 import intelligent_pay.bankbookservice.exception.returnBool.BindingCustomBoolException;
 import intelligent_pay.bankbookservice.exception.BindingCustomException;
+import intelligent_pay.bankbookservice.repository.BankbookRepository;
+import intelligent_pay.bankbookservice.utility.CommonUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 
 import java.util.Objects;
 
 @Component
+@RequiredArgsConstructor
 public class ControllerValidator {
+
+    private final BankbookRepository bankbookRepository;
 
     public void validateBinding(BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -25,6 +33,18 @@ public class ControllerValidator {
                     .requireNonNull(bindingResult.getFieldError())
                     .getDefaultMessage();
             throw new BindingCustomBoolException(errorMessage);
+        }
+    }
+
+    public void validateUsername(String username, String bankbookNum) {
+        String foundUsername = bankbookRepository.findUsernameByBankbookNum(bankbookNum);
+
+        if (CommonUtils.isNull(foundUsername)) {
+            throw new BankbookCustomException(ResponseMessage.NOT_EXIST_MEMBER);
+        }
+
+        if (!foundUsername.equals(username)) {
+            throw new BankbookCustomException(ResponseMessage.USERNAME_NOT_MATCH);
         }
     }
 }
