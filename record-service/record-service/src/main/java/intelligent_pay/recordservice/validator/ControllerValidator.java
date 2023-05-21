db400def3cf56bdccb52caf6d4ca337598c87562
liveforone <yc4852@gmail.com>
2023-05-21 21:19:54 +0900

    Remove throw bool validator.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/validator/ControllerValidator.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/validator/ControllerValidator.java
@@ -1,7 +1,6 @@
 package intelligent_pay.recordservice.validator;
 
 import intelligent_pay.recordservice.exception.BindingCustomException;
-import intelligent_pay.recordservice.exception.returnBool.BindingCustomBoolException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 
@@ -16,12 +15,6 @@ public class ControllerValidator {
                     .requireNonNull(bindingResult.getFieldError())
                     .getDefaultMessage();
             throw new BindingCustomException(errorMessage);
-        }
-    }
-
-    public void validateBindingThrowBool(BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            throw new BindingCustomBoolException();
         }
     }
 }
