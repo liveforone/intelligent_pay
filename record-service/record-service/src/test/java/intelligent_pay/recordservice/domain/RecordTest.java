b48196e973344b44c9be332be65898d06ef97964
liveforone <yc4852@gmail.com>
2023-05-21 18:43:17 +0900

    Add record domain test.

---
+++ record-service/record-service/src/test/java/intelligent_pay/recordservice/domain/RecordTest.java
@@ -0,0 +1,17 @@
+package intelligent_pay.recordservice.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecordTest {
+
+    @Test
+    void createTest() {
+
+    }
+
+    @Test
+    void cancelStateTest() {
+    }
+}
