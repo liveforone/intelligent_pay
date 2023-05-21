86436f536f0ea7fba510ecfa727917f04a5cacdc
liveforone <yc4852@gmail.com>
2023-05-21 18:50:50 +0900

    Remove kafka logic.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/kafka/KafkaServer.java
+++
@@ -1,7 +0,0 @@
-package intelligent_pay.recordservice.kafka;
-
-public final class KafkaServer {
-
-    public static final String KAFKA_SERVER_POST = "localhost:9092";
-//    public static final String KAFKA_SERVER_POST = "172.18.0.101:9092";
-}
--- record-service/record-service/src/main/java/intelligent_pay/recordservice/kafka/Topic.java
+++
@@ -1,7 +0,0 @@
-package intelligent_pay.recordservice.kafka;
-
-public final class Topic {
-
-    private Topic() {}
-    public static final String REMOVE_RECORD_BELONG_USER = "remove-record-belong-user";
-}
