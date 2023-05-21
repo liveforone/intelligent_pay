9f64e7517b316cc036e027a939bf3af8f02740a7
liveforone <yc4852@gmail.com>
2023-05-21 21:20:06 +0900

    Remove throw bool controller advice.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/exception/RecordControllerAdvice.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/exception/RecordControllerAdvice.java
@@ -1,7 +1,5 @@
 package intelligent_pay.recordservice.exception;
 
-import intelligent_pay.recordservice.exception.returnBool.BindingCustomBoolException;
-import intelligent_pay.recordservice.exception.returnBool.RecordCustomBoolException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,15 +13,5 @@ public class RecordControllerAdvice {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(customException.getMessage());
-    }
-
-    @ExceptionHandler(BindingCustomBoolException.class)
-    protected boolean bindingBoolErrorHandle(BindingCustomBoolException bindingCustomBoolException) {
-        return false;
-    }
-
-    @ExceptionHandler(RecordCustomBoolException.class)
-    protected boolean recordCustomBoolHandle(RecordCustomBoolException recordCustomBoolException) {
-        return false;
     }
 }
