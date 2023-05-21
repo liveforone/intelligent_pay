7578ad7838e34baa661a1dfe4556a35ab182e7cf
liveforone <yc4852@gmail.com>
2023-05-21 18:07:26 +0900

    Add create withdraw record controller log.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/controller/constant/ControllerLog.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/controller/constant/ControllerLog.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ControllerLog {
 
-    CREATE_DEPOSIT_RECORD("입금 거래내역을 생성하였습니다. | ID : ");
+    CREATE_DEPOSIT_RECORD("입금 거래내역을 생성하였습니다. | ID : "),
+    CREATE_WITHDRAW_RECORD("출금 거래내역을 생성하였습니다. | ID : ");
 
     private final String value;
 }
