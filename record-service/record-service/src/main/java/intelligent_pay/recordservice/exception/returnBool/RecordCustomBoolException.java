bbf36b8a3b8f07371ba2260f5340ad56213d1b40
liveforone <yc4852@gmail.com>
2023-05-21 17:45:21 +0900

    Add record custom bool exception.

---
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/exception/returnBool/RecordCustomBoolException.java
@@ -0,0 +1,7 @@
+package intelligent_pay.recordservice.exception.returnBool;
+
+public class RecordCustomBoolException extends RuntimeException {
+    public RecordCustomBoolException() {
+        super();
+    }
+}
