9774d796e842551354012ffa47dc62cad03bddf9
liveforone <yc4852@gmail.com>
2023-05-21 17:53:21 +0900

    Add create deposit record controller log.

---
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/controller/constant/ControllerLog.java
@@ -0,0 +1,13 @@
+package intelligent_pay.recordservice.controller.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ControllerLog {
+
+    CREATE_DEPOSIT_RECORD("입금 거래내역을 생성하였습니다. | ID : ");
+
+    private final String value;
+}
