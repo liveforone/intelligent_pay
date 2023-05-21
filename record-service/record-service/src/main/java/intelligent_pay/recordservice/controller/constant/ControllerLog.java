ec2a7ed7069a3b02a3d83910e74202753b78ac47
liveforone <yc4852@gmail.com>
2023-05-21 18:41:36 +0900

    Add cancel record success controller log.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/controller/constant/ControllerLog.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/controller/constant/ControllerLog.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ControllerLog {
 
     CREATE_DEPOSIT_RECORD("입금 거래내역을 생성하였습니다. | ID : "),
-    CREATE_WITHDRAW_RECORD("출금 거래내역을 생성하였습니다. | ID : ");
+    CREATE_WITHDRAW_RECORD("출금 거래내역을 생성하였습니다. | ID : "),
+    CANCEL_RECORD_SUCCESS("거래내역을 취소하였습니다.");
 
     private final String value;
 }
