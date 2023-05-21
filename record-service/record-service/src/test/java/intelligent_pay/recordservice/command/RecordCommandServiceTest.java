0cd818a2efcc004c684dbba46fe2f6c163d691c8
liveforone <yc4852@gmail.com>
2023-05-21 21:17:53 +0900

    Remove cancel state service test.

--- record-service/record-service/src/test/java/intelligent_pay/recordservice/command/RecordCommandServiceTest.java
+++ record-service/record-service/src/test/java/intelligent_pay/recordservice/command/RecordCommandServiceTest.java
@@ -1,7 +1,5 @@
 package intelligent_pay.recordservice.command;
 
-import intelligent_pay.recordservice.domain.RecordState;
-import intelligent_pay.recordservice.dto.CancelStateRequest;
 import intelligent_pay.recordservice.dto.RecordRequest;
 import intelligent_pay.recordservice.query.RecordQueryService;
 import jakarta.persistence.EntityManager;
@@ -66,46 +64,5 @@ class RecordCommandServiceTest {
         //then
         assertThat(recordQueryService.getRecordById(depositRecordId).getId())
                 .isEqualTo(depositRecordId);
-    }
-
-    @Test
-    @Transactional
-    void cancelStateTest() {
-        //given
-        String depositBankbookNum = "8128139239821";
-        long depositMoney = 4000;
-        String depositTitle = "홍길동 입금";
-        RecordRequest depositRequest = new RecordRequest();
-        depositRequest.setBankBookNum(depositBankbookNum);
-        depositRequest.setMoney(depositMoney);
-        depositRequest.setTitle(depositTitle);
-        Long depositRecordId = recordCommandService.createDepositRecord(depositRequest);
-        em.flush();
-        em.clear();
-
-        String withdrawBankbookNum = "1234569239821";
-        long withdrawMoney = 4000;
-        String withdrawTitle = "홍길동 출금";
-        RecordRequest withdrawRequest = new RecordRequest();
-        withdrawRequest.setBankBookNum(withdrawBankbookNum);
-        withdrawRequest.setMoney(withdrawMoney);
-        withdrawRequest.setTitle(withdrawTitle);
-        Long withdrawRecordId = recordCommandService.createWithdrawRecord(withdrawRequest);
-        em.flush();
-        em.clear();
-
-        //when
-        CancelStateRequest cancelRequest = new CancelStateRequest();
-        cancelRequest.setDepositRecordId(depositRecordId);
-        cancelRequest.setWithdrawRecordId(withdrawRecordId);
-        recordCommandService.cancelState(cancelRequest);
-        em.flush();
-        em.clear();
-
-        //then
-        assertThat(recordQueryService.getRecordById(depositRecordId).getRecordState())
-                .isEqualTo(RecordState.CANCEL);
-        assertThat(recordQueryService.getRecordById(withdrawRecordId).getRecordState())
-                .isEqualTo(RecordState.CANCEL);
     }
 }
