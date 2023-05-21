5baf4d95f959098ea42a8b738dc417018c1c2601
liveforone <yc4852@gmail.com>
2023-05-21 21:18:37 +0900

    Remove cancel state service method.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/command/RecordCommandService.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/command/RecordCommandService.java
@@ -2,10 +2,8 @@ package intelligent_pay.recordservice.command;
 
 import intelligent_pay.recordservice.domain.Record;
 import intelligent_pay.recordservice.domain.RecordState;
-import intelligent_pay.recordservice.dto.CancelStateRequest;
 import intelligent_pay.recordservice.dto.RecordRequest;
 import intelligent_pay.recordservice.repository.RecordRepository;
-import intelligent_pay.recordservice.validator.ServiceValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class RecordCommandService {
 
     private final RecordRepository recordRepository;
-    private final ServiceValidator serviceValidator;
 
     public Long createDepositRecord(RecordRequest requestDto) {
         Record record = Record.create(requestDto, RecordState.DEPOSIT);
@@ -26,19 +23,5 @@ public class RecordCommandService {
     public Long createWithdrawRecord(RecordRequest requestDto) {
         Record record = Record.create(requestDto, RecordState.WITHDRAW);
         return recordRepository.save(record).getId();
-    }
-
-    public void cancelState(CancelStateRequest requestDto) {
-        Long depositRecordId = requestDto.getDepositRecordId();
-        serviceValidator.validateRecordNull(depositRecordId);
-
-        Long withdrawRecordId = requestDto.getWithdrawRecordId();
-        serviceValidator.validateRecordNull(withdrawRecordId);
-
-        Record depositRecord = recordRepository.findOneById(depositRecordId);
-        depositRecord.cancelState();
-
-        Record withdrawRecord = recordRepository.findOneById(withdrawRecordId);
-        withdrawRecord.cancelState();
     }
 }
