1c91db7d700f90b52b8123050dfd0dd0ff1a31d2
liveforone <yc4852@gmail.com>
2023-05-21 21:19:24 +0900

    Remove custom bool exception.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/exception/returnBool/BindingCustomBoolException.java
+++
@@ -1,7 +0,0 @@
-package intelligent_pay.recordservice.exception.returnBool;
-
-public class BindingCustomBoolException extends RuntimeException {
-    public BindingCustomBoolException() {
-        super();
-    }
-}
--- record-service/record-service/src/main/java/intelligent_pay/recordservice/exception/returnBool/RecordCustomBoolException.java
+++
@@ -1,7 +0,0 @@
-package intelligent_pay.recordservice.exception.returnBool;
-
-public class RecordCustomBoolException extends RuntimeException {
-    public RecordCustomBoolException() {
-        super();
-    }
-}
