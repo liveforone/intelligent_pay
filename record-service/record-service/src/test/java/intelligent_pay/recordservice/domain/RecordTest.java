2def816d474dddbd2f53ea832403976bed934193
liveforone <yc4852@gmail.com>
2023-05-21 18:52:17 +0900

    Add record create test.

--- record-service/record-service/src/test/java/intelligent_pay/recordservice/domain/RecordTest.java
+++ record-service/record-service/src/test/java/intelligent_pay/recordservice/domain/RecordTest.java
@@ -1,14 +1,36 @@
 package intelligent_pay.recordservice.domain;
 
+import intelligent_pay.recordservice.dto.RecordRequest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
 
 class RecordTest {
 
     @Test
     void createTest() {
+        //given
+        String bankbookNum = "8128139239821";
+        long money = 4000;
+        String title = "홍길동 입금";
+        RecordRequest request = new RecordRequest();
+        request.setBankBookNum(bankbookNum);
+        request.setMoney(money);
+        request.setTitle(title);
 
+        //when
+        Record record = Record.create(request, RecordState.DEPOSIT);
+
+        //then
+        int nowYear = LocalDate.now().getYear();
+        assertThat(record.getCreatedYear())
+                .isEqualTo(nowYear);
+        assertThat(record.getRecordState())
+                .isEqualTo(RecordState.DEPOSIT);
+        assertThat(record.getTitle())
+                .isEqualTo(title);
     }
 
     @Test
