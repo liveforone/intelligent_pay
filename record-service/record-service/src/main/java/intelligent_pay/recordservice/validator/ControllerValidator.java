d43f0449ab4aceecde357441670530bd6cd60308
liveforone <yc4852@gmail.com>
2023-05-21 18:21:48 +0900

    Remove unusing dependency.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/validator/ControllerValidator.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/validator/ControllerValidator.java
@@ -2,18 +2,13 @@ package intelligent_pay.recordservice.validator;
 
 import intelligent_pay.recordservice.exception.BindingCustomException;
 import intelligent_pay.recordservice.exception.returnBool.BindingCustomBoolException;
-import intelligent_pay.recordservice.repository.RecordRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 
 import java.util.Objects;
 
 @Component
-@RequiredArgsConstructor
 public class ControllerValidator {
-
-    private final RecordRepository recordRepository;
 
     public void validateBinding(BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
