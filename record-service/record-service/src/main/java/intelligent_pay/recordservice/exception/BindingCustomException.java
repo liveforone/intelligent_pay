f99888a4917aa00ab2b08f14106406d31234911e
liveforone <yc4852@gmail.com>
2023-05-22 18:19:01 +0900

    Refactor change return type to bool.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/exception/BindingCustomException.java
+++
@@ -1,8 +0,0 @@
-package intelligent_pay.recordservice.exception;
-
-public class BindingCustomException extends RuntimeException{
-
-    public BindingCustomException(String message) {
-        super(message);
-    }
-}
---
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/exception/BindingCustomExceptionBool.java
@@ -0,0 +1,8 @@
+package intelligent_pay.recordservice.exception;
+
+public class BindingCustomExceptionBool extends RuntimeException{
+
+    public BindingCustomExceptionBool() {
+        super();
+    }
+}
