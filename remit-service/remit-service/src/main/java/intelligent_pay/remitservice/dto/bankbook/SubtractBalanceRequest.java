01745ba21323829ad0073914ab92c4b5dd728234
liveforone <yc4852@gmail.com>
2023-05-23 13:34:13 +0900

    Add builder.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/dto/bankbook/SubtractBalanceRequest.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/dto/bankbook/SubtractBalanceRequest.java
@@ -1,10 +1,14 @@
 package intelligent_pay.remitservice.dto.bankbook;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class SubtractBalanceRequest {
 
     private String bankbookNum;
