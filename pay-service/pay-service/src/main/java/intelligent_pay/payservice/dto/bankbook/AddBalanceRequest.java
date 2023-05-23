2157c128bc8d5e57738028d2b7e3e5a8123aabea
liveforone <yc4852@gmail.com>
2023-05-23 15:39:29 +0900

    Add addBalance request dto.

---
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/dto/bankbook/AddBalanceRequest.java
@@ -0,0 +1,16 @@
+package intelligent_pay.payservice.dto.bankbook;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddBalanceRequest {
+
+    private String bankbookNum;
+    private long money;
+}
