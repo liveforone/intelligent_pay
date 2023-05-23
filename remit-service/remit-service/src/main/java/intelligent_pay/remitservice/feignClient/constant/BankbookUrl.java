59ad7aa28a11efd5a51b0060c785f029a7dc1e15
liveforone <yc4852@gmail.com>
2023-05-23 12:37:18 +0900

    Add bankbook url.

---
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/feignClient/constant/BankbookUrl.java
@@ -0,0 +1,9 @@
+package intelligent_pay.remitservice.feignClient.constant;
+
+public final class BankbookUrl {
+    private BankbookUrl() {}
+
+    public static final String BASE = "bankbook-service";
+    public static final String ADD_BALANCE = "/provide/add/balance";
+    public static final String SUBTRACT_BALANCE = "/provide/subtract/balance";
+}
