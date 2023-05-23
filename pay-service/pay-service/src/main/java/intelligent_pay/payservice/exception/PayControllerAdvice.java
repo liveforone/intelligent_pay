d49d06aad02767840e939dfef8234091d954546c
liveforone <yc4852@gmail.com>
2023-05-23 15:41:52 +0900

    Add pay controller advice.

---
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/exception/PayControllerAdvice.java
@@ -0,0 +1,24 @@
+package intelligent_pay.payservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class PayControllerAdvice {
+
+    @ExceptionHandler(PayCustomException.class)
+    protected ResponseEntity<?> payCustomHandle(PayCustomException customException) {
+        return ResponseEntity
+                .status(customException.getResponseMessage().getStatus())
+                .body(customException.getResponseMessage().getValue());
+    }
+
+    @ExceptionHandler(BindingCustomException.class)
+    protected ResponseEntity<?> bindingErrorHandle(BindingCustomException customException) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(customException.getMessage());
+    }
+}
