1ba2a335d139f514201de2aa727f8185d19bdade
liveforone <yc4852@gmail.com>
2023-05-23 15:40:41 +0900

    Add record url.

---
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/feignClient/constant/RecordUrl.java
@@ -0,0 +1,9 @@
+package intelligent_pay.payservice.feignClient.constant;
+
+public final class RecordUrl {
+    private RecordUrl() {}
+
+    public static final String BASE = "record-service";
+    public static final String DEPOSIT_RECORD = "/provide/record/deposit";
+    public static final String WITHDRAW_RECORD = "/provide/record/withdraw";
+}
