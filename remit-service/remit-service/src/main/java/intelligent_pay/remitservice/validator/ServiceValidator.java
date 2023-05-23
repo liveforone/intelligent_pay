eed6a1dcdedf1a31522e95ac9e34a6973180c358
liveforone <yc4852@gmail.com>
2023-05-23 13:33:56 +0900

    Add service validator. and add validate add balance and deposit record validator.

---
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/validator/ServiceValidator.java
@@ -0,0 +1,21 @@
+package intelligent_pay.remitservice.validator;
+
+import intelligent_pay.remitservice.controller.restResponse.ResponseMessage;
+import intelligent_pay.remitservice.exception.RemitCustomException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceValidator {
+
+    public void validateAddBalance(Boolean addBalanceResult) {
+        if (!addBalanceResult) {
+            throw new RemitCustomException(ResponseMessage.DEPOSIT_FAIL);
+        }
+    }
+
+    public void validateDepositRecord(Boolean depositRecordResult) {
+        if (!depositRecordResult) {
+            throw new RemitCustomException(ResponseMessage.DEPOSIT_RECORD_FAIL);
+        }
+    }
+}
