e59dfe5b081de6add425e52da24c430a7b6114da
liveforone <yc4852@gmail.com>
2023-05-23 12:45:42 +0900

    Add validation in requet dto.

---
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/dto/remit/RemitRequest.java
@@ -0,0 +1,27 @@
+package intelligent_pay.remitservice.dto.remit;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class RemitRequest {
+
+    @NotBlank(message = "입금 통장번호를 입력해주세요.")
+    @Size(min = 13, message = "계좌번호 양식이 올바르지 않습니다.")
+    private String bankbookNum;
+
+    @Positive(message = "입금 금액은 양수로 입력해주세요")
+    private long money;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 8)
+    private String password;
+
+    @NotBlank(message = "출금 통장번호를 입력해주세요.")
+    @Size(min = 13, message = "계좌번호 양식이 올바르지 않습니다.")
+    private String otherBankbookNum;
+}
