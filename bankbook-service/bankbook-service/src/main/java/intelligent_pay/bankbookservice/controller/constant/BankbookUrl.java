76cf194b7138ee0c97cbee7f4c2597cd3db3e594
liveforone <yc4852@gmail.com>
2023-05-23 11:28:12 +0900

    Add provide path in provide api.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/BankbookUrl.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/BankbookUrl.java
@@ -6,8 +6,8 @@ public final class BankbookUrl {
     public static final String BASIC_INFO = "/basic/info/{username}";
     public static final String INFO = "/info";
     public static final String CREATE = "/create";
-    public static final String ADD_BALANCE = "/add/balance";
-    public static final String SUBTRACT_BALANCE = "/subtract/balance";
+    public static final String ADD_BALANCE = "/provide/add/balance";
+    public static final String SUBTRACT_BALANCE = "/provide/subtract/balance";
     public static final String UPDATE_PASSWORD = "/update/password";
     public static final String SUSPEND = "/suspend";
     public static final String CANCEL_SUSPEND = "/cancel/suspend";
