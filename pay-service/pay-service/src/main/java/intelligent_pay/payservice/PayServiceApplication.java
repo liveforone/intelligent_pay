9bdf0292e14c116efb3c0ca9161a25767483ca94
liveforone <yc4852@gmail.com>
2023-05-23 14:18:40 +0900

    Initial pay-service.

---
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/PayServiceApplication.java
@@ -0,0 +1,29 @@
+package intelligent_pay.payservice;
+
+import feign.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class PayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayServiceApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLeve() {
+		return Logger.Level.FULL;
+	}
+}
