e0a8a0917ab413ecba83cdefdba40bcdbc6aa73a
liveforone <yc4852@gmail.com>
2023-05-23 12:46:06 +0900

    Add remit controller advice.

---
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/exception/RemitControllerAdvice.java
@@ -0,0 +1,24 @@
+package intelligent_pay.remitservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RemitControllerAdvice {
+
+    @ExceptionHandler(RemitCustomException.class)
+    protected ResponseEntity<?> remitCustomHandle(RemitCustomException customException) {
+        return ResponseEntity
+                .status(customException.getResponseMessage().getStatus())
+                .body(customException.getResponseMessage().getValue());
+    }
+
+    @ExceptionHandler(BindingCustomException.class)
+    protected ResponseEntity<?> bindingErrorHandle(BindingCustomException customException) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(customException.getMessage());
+    }
+}
