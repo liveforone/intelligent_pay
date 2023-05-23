4f855520a262a7c4bdac5e2cbc07c1e9a2f706e8
liveforone <yc4852@gmail.com>
2023-05-23 12:37:26 +0900

    Add bankbook feign service.

---
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/feignClient/BankbookFeignService.java
@@ -0,0 +1,19 @@
+package intelligent_pay.remitservice.feignClient;
+
+import intelligent_pay.remitservice.dto.bankbook.AddBalanceRequest;
+import intelligent_pay.remitservice.dto.bankbook.SubtractBalanceRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import static intelligent_pay.remitservice.feignClient.constant.BankbookUrl.*;
+
+@FeignClient(name = BASE)
+public interface BankbookFeignService {
+
+    @PostMapping(ADD_BALANCE)
+    boolean deposit(@RequestBody AddBalanceRequest requestDto);
+
+    @PostMapping(SUBTRACT_BALANCE)
+    boolean withdraw(@RequestBody SubtractBalanceRequest requestDto);
+}
