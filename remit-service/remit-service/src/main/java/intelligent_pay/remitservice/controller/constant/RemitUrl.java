02ba59ad3210cfc387978be4531cf493ac01c750
liveforone <yc4852@gmail.com>
2023-05-23 12:53:33 +0900

    Add remit and deposit url.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/controller/constant/RemitUrl.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/controller/constant/RemitUrl.java
@@ -1,5 +1,6 @@
 package intelligent_pay.remitservice.controller.constant;
 
 public final class RemitUrl {
-
+    public static final String DEPOSIT = "/deposit";
+    public static final String REMIT = "/remit";
 }
