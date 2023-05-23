94144ff34bf730811d3a48fd37efbcec91bd4cf3
liveforone <yc4852@gmail.com>
2023-05-23 13:32:43 +0900

    Add builder.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/dto/record/RecordRequest.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/dto/record/RecordRequest.java
@@ -1,10 +1,14 @@
 package intelligent_pay.remitservice.dto.record;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class RecordRequest {
 
     private String title;
