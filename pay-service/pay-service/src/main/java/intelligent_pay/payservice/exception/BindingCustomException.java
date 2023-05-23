9d455e4336baa3c09070d5368164d0f1408a1097
liveforone <yc4852@gmail.com>
2023-05-23 14:20:06 +0900

    Add binding custom exception.

---
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/exception/BindingCustomException.java
@@ -0,0 +1,8 @@
+package intelligent_pay.payservice.exception;
+
+public class BindingCustomException extends RuntimeException{
+
+    public BindingCustomException(String message) {
+        super(message);
+    }
+}
