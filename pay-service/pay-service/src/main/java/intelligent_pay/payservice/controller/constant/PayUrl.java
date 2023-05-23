86c097722262a97028506626d75f03beb23eff91
liveforone <yc4852@gmail.com>
2023-05-23 14:22:18 +0900

    Add pay url.

---
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/controller/constant/PayUrl.java
@@ -0,0 +1,6 @@
+package intelligent_pay.payservice.controller.constant;
+
+public final class PayUrl {
+    public static final String PAY = "/pay";
+    public static final String CANCEL_PAY = "/cancel/pay";
+}
