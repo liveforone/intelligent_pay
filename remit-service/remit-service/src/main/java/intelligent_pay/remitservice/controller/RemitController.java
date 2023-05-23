7307075c8a316e285f13cab547e34cc3acd42222
liveforone <yc4852@gmail.com>
2023-05-23 13:34:40 +0900

    Add deposit controller method.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/controller/RemitController.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/controller/RemitController.java
@@ -1,13 +1,35 @@
 package intelligent_pay.remitservice.controller;
 
+import intelligent_pay.remitservice.command.RemitCommandService;
+import intelligent_pay.remitservice.controller.restResponse.RestResponse;
+import intelligent_pay.remitservice.dto.remit.DepositRequest;
+import intelligent_pay.remitservice.validator.ControllerValidator;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static intelligent_pay.remitservice.controller.constant.RemitUrl.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class RemitController {
 
+    private final RemitCommandService remitCommandService;
+    private final ControllerValidator controllerValidator;
 
+    @PostMapping(DEPOSIT)
+    public ResponseEntity<?> deposit(
+            @RequestBody @Valid DepositRequest requestDto,
+            BindingResult bindingResult
+    ) {
+        controllerValidator.validateBinding(bindingResult);
+        remitCommandService.deposit(requestDto);
+        return RestResponse.depositSuccess();
+    }
 }
