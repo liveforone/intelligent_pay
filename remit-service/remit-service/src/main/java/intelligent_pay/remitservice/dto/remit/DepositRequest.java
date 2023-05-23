5425fe9a459d00ba77a4b6c01ea9aa43bd548cd0
liveforone <yc4852@gmail.com>
2023-05-23 13:13:07 +0900

    Add other bankbookNum filed.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/dto/remit/DepositRequest.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/dto/remit/DepositRequest.java
@@ -16,4 +16,8 @@ public class DepositRequest {
 
     @Positive(message = "입금 금액은 양수로 입력해주세요")
     private long money;
+
+    @NotBlank(message = "입금자 통장번호를 입력해주세요.")
+    @Size(min = 13, message = "계좌번호 양식이 올바르지 않습니다.")
+    private String otherBankbookNum;
 }
