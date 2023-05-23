c010d039c7f085d1e2b65c2543a784daa84fba37
liveforone <yc4852@gmail.com>
2023-05-23 13:54:20 +0900

    Add remit message.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/controller/restResponse/ResponseMessage.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/controller/restResponse/ResponseMessage.java
@@ -9,6 +9,8 @@ public enum ResponseMessage {
 
     DEPOSIT_FAIL(400, "입금에 실패하였습니다."),
     DEPOSIT_RECORD_FAIL(400, "입금 거래내역 생성에 실패하였습니다."),
+    REMIT_FAIL(400, "송금에 실패하였습니다."),
+    REMIT_RECORD_FAIL(400, "송금 거래내역 생성에 실패하였습니다."),
     DEPOSIT_SUCCESS(201, "입금을 완료하였습니다."),
     REMIT_SUCCESS(201, "송금을 완료하였습니다.");
 
