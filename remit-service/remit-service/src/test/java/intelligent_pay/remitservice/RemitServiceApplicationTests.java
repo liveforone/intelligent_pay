0931f469c45147e10f949fec3711220d2ff2a33b
liveforone <yc4852@gmail.com>
2023-05-23 11:42:18 +0900

    Initial remit-service.

---
+++ remit-service/remit-service/src/test/java/intelligent_pay/remitservice/RemitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package intelligent_pay.remitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
