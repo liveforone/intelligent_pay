dd536abd545295b4a0b6db470156be8e0c1124a4
liveforone <yc4852@gmail.com>
2023-05-23 13:55:10 +0900

    Add validate subtract balance and record validator.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/validator/ServiceValidator.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/validator/ServiceValidator.java
@@ -18,4 +18,16 @@ public class ServiceValidator {
             throw new RemitCustomException(ResponseMessage.DEPOSIT_RECORD_FAIL);
         }
     }
+
+    public void validateSubtractBalance(Boolean subtractBalanceResult) {
+        if (!subtractBalanceResult) {
+            throw new RemitCustomException(ResponseMessage.REMIT_FAIL);
+        }
+    }
+
+    public void validateWithdrawRecord(Boolean withdrawRecordResult) {
+        if (!withdrawRecordResult) {
+            throw new RemitCustomException(ResponseMessage.REMIT_RECORD_FAIL);
+        }
+    }
 }
