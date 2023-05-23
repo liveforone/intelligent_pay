60f8e2d1baa2ee72025b623090290a6e7befd9b8
liveforone <yc4852@gmail.com>
2023-05-23 13:54:47 +0900

    Add remit service method..

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/command/RemitCommandService.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/command/RemitCommandService.java
@@ -1,8 +1,10 @@
 package intelligent_pay.remitservice.command;
 
 import intelligent_pay.remitservice.dto.bankbook.AddBalanceRequest;
+import intelligent_pay.remitservice.dto.bankbook.SubtractBalanceRequest;
 import intelligent_pay.remitservice.dto.record.RecordRequest;
 import intelligent_pay.remitservice.dto.remit.DepositRequest;
+import intelligent_pay.remitservice.dto.remit.RemitRequest;
 import intelligent_pay.remitservice.feignClient.BankbookFeignService;
 import intelligent_pay.remitservice.feignClient.RecordFeignService;
 import intelligent_pay.remitservice.feignClient.constant.CircuitLog;
@@ -51,6 +53,57 @@ public class RemitCommandService {
                 .create(CircuitLog.REMIT_CIRCUIT_LOG.getValue())
                 .run(
                         () -> recordFeignService.depositRecord(requestDto),
+                        throwable -> false
+                );
+    }
+
+    public void remit(RemitRequest requestDto) {
+        SubtractBalanceRequest subtractRequest = SubtractBalanceRequest.builder()
+                .bankbookNum(requestDto.getBankbookNum())
+                .money(requestDto.getMoney())
+                .password(requestDto.getPassword())
+                .build();
+        boolean subtractBalanceResult = subtractBalance(subtractRequest);
+        serviceValidator.validateSubtractBalance(subtractBalanceResult);
+
+        AddBalanceRequest addRequest = AddBalanceRequest.builder()
+                .bankbookNum(requestDto.getOtherBankbookNum())
+                .money(requestDto.getMoney())
+                .build();
+        boolean addBalanceResult = addBalance(addRequest);
+        serviceValidator.validateAddBalance(addBalanceResult);
+
+        RecordRequest withdrawRequest = RecordRequest.builder()
+                .title(requestDto.getOtherBankbookNum() + "출금")
+                .bankBookNum(requestDto.getBankbookNum())
+                .money(requestDto.getMoney())
+                .build();
+        boolean withdrawRecordResult = withdrawRecord(withdrawRequest);
+        serviceValidator.validateWithdrawRecord(withdrawRecordResult);
+
+        RecordRequest depositRequest = RecordRequest.builder()
+                .title(requestDto.getBankbookNum() + "입금")
+                .bankBookNum(requestDto.getOtherBankbookNum())
+                .money(requestDto.getMoney())
+                .build();
+        boolean depositRecordResult = depositRecord(depositRequest);
+        serviceValidator.validateDepositRecord(depositRecordResult);
+    }
+
+    private boolean subtractBalance(SubtractBalanceRequest requestDto) {
+        return circuitBreakerFactory
+                .create(CircuitLog.REMIT_CIRCUIT_LOG.getValue())
+                .run(
+                        () -> bankbookFeignService.subtractBalance(requestDto),
+                        throwable -> false
+                );
+    }
+
+    private boolean withdrawRecord(RecordRequest requestDto) {
+        return circuitBreakerFactory
+                .create(CircuitLog.REMIT_CIRCUIT_LOG.getValue())
+                .run(
+                        () -> recordFeignService.withdrawRecord(requestDto),
                         throwable -> false
                 );
     }
