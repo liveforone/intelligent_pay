0f4c2754c657e69b8527d099341d04e0a79733ec
liveforone <yc4852@gmail.com>
2023-05-23 15:40:28 +0900

    Add pay and cancel pay success respone message.

---
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/controller/restResponse/ResponseMessage.java
@@ -0,0 +1,19 @@
+package intelligent_pay.payservice.controller.restResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ResponseMessage {
+
+    PAY_FAIL(400, "결제에 실패하였습니다."),
+    PAY_RECORD_FAIL(400, "결제 거래내역 생성에 실패하였습니다."),
+    CANCEL_PAY_FAIL(400, "결제 취소에 실패하였습니다."),
+    CANCEL_PAY_RECORD_FAIL(400, "결제 취소 거래내역 생성에 실패하였습니다."),
+    PAY_SUCCESS(201, "결제를 완료하였습니다."),
+    CANCEL_PAY_SUCCESS(201, "결제취소를 완료하였습니다.");
+
+    private final int status;
+    private final String value;
+}
