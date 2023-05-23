ed62952a890c34596beaaedaa3fa2858b1cabc78
liveforone <yc4852@gmail.com>
2023-05-23 12:32:08 +0900

    Add binding custom exception.

---
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/exception/BindingCustomException.java
@@ -0,0 +1,8 @@
+package intelligent_pay.remitservice.exception;
+
+public class BindingCustomException extends RuntimeException{
+
+    public BindingCustomException(String message) {
+        super(message);
+    }
+}
