637d25677a95b713b57c323e8021d09ff693c3c3
liveforone <yc4852@gmail.com>
2023-05-28 17:37:46 +0900

    Add kafka topic.

---
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/kafka/Topic.java
@@ -0,0 +1,9 @@
+package intelligent_pay.recordservice.kafka;
+
+public final class Topic {
+
+    private Topic() {}
+
+    public static final String ADD_BALANCE = "add-balance";
+    public static final String SUBTRACT_BALANCE = "subtract-balance";
+}
