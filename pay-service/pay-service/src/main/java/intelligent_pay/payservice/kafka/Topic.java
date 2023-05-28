aa187085f9b376da30e7f2431402d318921cf00b
liveforone <yc4852@gmail.com>
2023-05-28 19:23:41 +0900

    Add kafka topic.

---
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/kafka/Topic.java
@@ -0,0 +1,9 @@
+package intelligent_pay.payservice.kafka;
+
+public final class Topic {
+
+    private Topic() {}
+
+    public static final String DEPOSIT_RECORD = "deposit-record";
+    public static final String WITHDRAW_RECORD = "withdraw-record";
+}
