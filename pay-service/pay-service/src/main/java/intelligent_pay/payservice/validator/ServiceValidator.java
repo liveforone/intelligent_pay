aa6f873121b1029d40e1c45437a0fb59a8be7a03
liveforone <yc4852@gmail.com>
2023-05-28 19:41:57 +0900

    Remove record validator.

--- pay-service/pay-service/src/main/java/intelligent_pay/payservice/validator/ServiceValidator.java
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/validator/ServiceValidator.java
@@ -13,21 +13,9 @@ public class ServiceValidator {
         }
     }
 
-    public void validatePayRecord(Boolean payRecordResult) {
-        if (!payRecordResult) {
-            throw new PayCustomException(ResponseMessage.PAY_RECORD_FAIL);
-        }
-    }
-
     public void validateCancelPay(Boolean cancelPayResult) {
         if (!cancelPayResult) {
             throw new PayCustomException(ResponseMessage.CANCEL_PAY_FAIL);
-        }
-    }
-
-    public void validateCancelPayRecord(Boolean cancelPayRecordResult) {
-        if (!cancelPayRecordResult) {
-            throw new PayCustomException(ResponseMessage.CANCEL_PAY_RECORD_FAIL);
         }
     }
 }
