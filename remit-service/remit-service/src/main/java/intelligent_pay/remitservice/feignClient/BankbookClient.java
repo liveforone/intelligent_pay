cf1e8db88c2c859a177516af5b27ea2ce84794d4
liveforone <yc4852@gmail.com>
2023-05-28 17:16:41 +0900

    Refactor rename bankbook client.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/feignClient/BankbookClient.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/feignClient/BankbookClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import static intelligent_pay.remitservice.feignClient.constant.BankbookUrl.*;
 
 @FeignClient(name = BANKBOOK_BASE)
-public interface BankbookFeignService {
+public interface BankbookClient {
 
     @PostMapping(ADD_BALANCE)
     boolean addBalance(@RequestBody AddBalanceRequest requestDto);
