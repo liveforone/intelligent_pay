ea02a4b7df1c2db19a07e9131e10261399393fd1
liveforone <yc4852@gmail.com>
2023-05-28 19:14:45 +0900

    Remove record validator.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/validator/ServiceValidator.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/validator/ServiceValidator.java
@@ -13,21 +13,9 @@ public class ServiceValidator {
         }
     }
 
-    public void validateDepositRecord(Boolean depositRecordResult) {
-        if (!depositRecordResult) {
-            throw new RemitCustomException(ResponseMessage.DEPOSIT_RECORD_FAIL);
-        }
-    }
-
     public void validateSubtractBalance(Boolean subtractBalanceResult) {
         if (!subtractBalanceResult) {
             throw new RemitCustomException(ResponseMessage.REMIT_FAIL);
-        }
-    }
-
-    public void validateWithdrawRecord(Boolean withdrawRecordResult) {
-        if (!withdrawRecordResult) {
-            throw new RemitCustomException(ResponseMessage.REMIT_RECORD_FAIL);
         }
     }
 }
