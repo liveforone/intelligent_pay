c041aabf8dbd6ef88466e37ad8c1bd95fcb53b68
liveforone <yc4852@gmail.com>
2023-05-30 15:46:31 +0900

    Refactor security config csrf.

--- user-service/user-service/src/main/java/intelligent_pay/userservice/config/SecurityConfig.java
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,26 +31,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .addFilterBefore(
-                        new JwtAuthenticationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class
-                )
-                .sessionManagement(
-                        (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(
-                                MemberUrl.SIGNUP,
-                                MemberUrl.LOGIN,
-                                MemberUrl.ACTUATOR
-                        ).permitAll()
-                        .requestMatchers(
-                                MemberUrl.ADMIN_SEARCH
-                        ).hasRole(ADMIN_ROLE)
-                        .anyRequest().authenticated()
-                )
-                .exceptionHandling((exception) ->
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers(
+                        MemberUrl.SIGNUP,
+                        MemberUrl.LOGIN,
+                        MemberUrl.ACTUATOR
+                ).permitAll()
+                .requestMatchers(
+                        MemberUrl.ADMIN_SEARCH
+                ).hasRole(ADMIN_ROLE)
+                .anyRequest().authenticated()
+        );
+        http.sessionManagement(
+                (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+        http.addFilterBefore(
+                new JwtAuthenticationFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter.class
+        );
+        http.exceptionHandling((exception) ->
                         exception.accessDeniedPage(MemberUrl.PROHIBITION));
         return http.build();
     }
