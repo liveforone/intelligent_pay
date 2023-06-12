6da780f9c1d5b7a6ea326e77c2340c19fcf69c2e
liveforone <yc4852@gmail.com>
2023-06-12 19:24:16 +0900

    Remove null check in mapper. Cause already null check.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/query/util/BankbookMapper.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/query/util/BankbookMapper.java
@@ -2,14 +2,10 @@ package intelligent_pay.bankbookservice.query.util;
 
 import intelligent_pay.bankbookservice.domain.Bankbook;
 import intelligent_pay.bankbookservice.dto.response.BankbookResponse;
-import intelligent_pay.bankbookservice.utility.CommonUtils;
 
 public class BankbookMapper {
 
     public static BankbookResponse entityToDto(Bankbook bankbook) {
-        if (CommonUtils.isNull(bankbook)) {
-            return new BankbookResponse();
-        }
         return BankbookResponse.builder()
                 .id(bankbook.getId())
                 .balance(bankbook.getBalance())
