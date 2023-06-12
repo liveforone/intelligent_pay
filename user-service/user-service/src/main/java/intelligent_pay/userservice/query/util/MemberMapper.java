c6ce1ee78a70a0452142ab1af5a9302267acc552
liveforone <yc4852@gmail.com>
2023-06-12 19:20:52 +0900

    Remove null check in mapper. Cause already null check.

--- user-service/user-service/src/main/java/intelligent_pay/userservice/query/util/MemberMapper.java
+++ user-service/user-service/src/main/java/intelligent_pay/userservice/query/util/MemberMapper.java
@@ -2,15 +2,10 @@ package intelligent_pay.userservice.query.util;
 
 import intelligent_pay.userservice.domain.Member;
 import intelligent_pay.userservice.dto.response.MemberResponse;
-import intelligent_pay.userservice.utility.CommonUtils;
 
 public class MemberMapper {
 
     public static MemberResponse entityToDto(Member member) {
-        if (CommonUtils.isNull(member)) {
-            return new MemberResponse();
-        }
-
         return MemberResponse.builder()
                 .id(member.getId())
                 .email(member.getEmail())
