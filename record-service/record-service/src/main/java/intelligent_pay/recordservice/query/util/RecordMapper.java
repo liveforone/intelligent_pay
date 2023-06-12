5a7ef69aea612eb0a209fa2b38a40391167e6171
liveforone <yc4852@gmail.com>
2023-06-12 19:23:30 +0900

    Remove null check in mapper. Cause already null check.

--- record-service/record-service/src/main/java/intelligent_pay/recordservice/query/util/RecordMapper.java
+++ record-service/record-service/src/main/java/intelligent_pay/recordservice/query/util/RecordMapper.java
@@ -2,15 +2,10 @@ package intelligent_pay.recordservice.query.util;
 
 import intelligent_pay.recordservice.domain.Record;
 import intelligent_pay.recordservice.dto.RecordResponse;
-import intelligent_pay.recordservice.utility.CommonUtils;
 
 public class RecordMapper {
 
     public static RecordResponse entityToDto(Record record) {
-        if (CommonUtils.isNull(record)) {
-            return new RecordResponse();
-        }
-
         return RecordResponse.builder()
                 .id(record.getId())
                 .title(record.getTitle())
