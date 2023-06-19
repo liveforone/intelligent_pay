2d9f0dab81e271c9fc216eda06f9d547160e4214
liveforone <yc4852@gmail.com>
2023-06-19 16:13:24 +0900

    Add method content.

--- bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/BankbookUrl.java
+++ bankbook-service/bankbook-service/src/main/java/intelligent_pay/bankbookservice/controller/constant/BankbookUrl.java
@@ -3,13 +3,18 @@ package intelligent_pay.bankbookservice.controller.constant;
 public final class BankbookUrl {
     private BankbookUrl() {}
 
+    //==GET==//
     public static final String BASIC_INFO = "/basic/info/{username}";
     public static final String INFO = "/info";
-    public static final String CREATE = "/create";
     public static final String CHECK_BANKBOOK = "/provide/check/bankbook/{bankbookNum}";
+
+    //==POST==//
+    public static final String CREATE = "/create";
     public static final String ADD_BALANCE = "/provide/add/balance";
     public static final String SUBTRACT_BALANCE = "/provide/subtract/balance";
     public static final String SUBTRACT_BALANCE_FOR_CANCEL = "/provide/subtract/balance/cancel";
+
+    //==PUT==//
     public static final String UPDATE_PASSWORD = "/update/password";
     public static final String SUSPEND = "/suspend";
     public static final String CANCEL_SUSPEND = "/cancel/suspend";
