7b041c7ede53e48e0f253de8c2ee8bf037fdd9b6
liveforone <yc4852@gmail.com>
2023-06-19 16:25:46 +0900

    Add aysnc in producer.

--- pay-service/pay-service/src/main/java/intelligent_pay/payservice/producer/model/RecordProducer.java
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/producer/model/RecordProducer.java
@@ -1,11 +1,13 @@
 package intelligent_pay.payservice.producer.model;
 
 import com.google.gson.Gson;
+import intelligent_pay.payservice.async.AsyncConstant;
 import intelligent_pay.payservice.dto.record.RecordRequest;
 import intelligent_pay.payservice.producer.log.ProducerLog;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import static intelligent_pay.payservice.producer.model.ProducerTopic.*;
@@ -18,6 +20,7 @@ public class RecordProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
     Gson gson = new Gson();
 
+    @Async(AsyncConstant.commandAsync)
     public void depositRecord(RecordRequest recordRequest) {
         String jsonOrder = gson.toJson(recordRequest);
         String topic = DEPOSIT_RECORD;
@@ -25,6 +28,7 @@ public class RecordProducer {
         log.info(ProducerLog.KAFKA_SEND_LOG.getValue() + topic);
     }
 
+    @Async(AsyncConstant.commandAsync)
     public void withdrawRecord(RecordRequest recordRequest) {
         String jsonOrder = gson.toJson(recordRequest);
         String topic = WITHDRAW_RECORD;
