40e22d42d410e97b8fea6f3095a0b51ba4662a5d
liveforone <yc4852@gmail.com>
2023-06-19 16:47:32 +0900

    Add success and fail content.

--- pay-service/pay-service/src/main/java/intelligent_pay/payservice/controller/restResponse/ResponseMessage.java
+++ pay-service/pay-service/src/main/java/intelligent_pay/payservice/controller/restResponse/ResponseMessage.java
@@ -7,12 +7,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
 
+    //==success==//
+    PAY_SUCCESS(201, "결제를 완료하였습니다."),
+    CANCEL_PAY_SUCCESS(201, "결제취소를 완료하였습니다."),
+
+    //==fail==//
     PAY_FAIL(400, "결제에 실패하였습니다."),
     PAY_RECORD_FAIL(400, "결제 거래내역 생성에 실패하였습니다."),
     CANCEL_PAY_FAIL(400, "결제 취소에 실패하였습니다."),
-    CANCEL_PAY_RECORD_FAIL(400, "결제 취소 거래내역 생성에 실패하였습니다."),
-    PAY_SUCCESS(201, "결제를 완료하였습니다."),
-    CANCEL_PAY_SUCCESS(201, "결제취소를 완료하였습니다.");
+    CANCEL_PAY_RECORD_FAIL(400, "결제 취소 거래내역 생성에 실패하였습니다.");
 
     private final int status;
     private final String value;
