8943921d3484ad6d4ac23ae7eba93caac6e6148d
liveforone <yc4852@gmail.com>
2023-06-19 16:46:22 +0900

    Add success and fail content.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/controller/restResponse/ResponseMessage.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/controller/restResponse/ResponseMessage.java
@@ -7,12 +7,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
 
+    //==success==//
+    DEPOSIT_SUCCESS(201, "입금을 완료하였습니다."),
+    REMIT_SUCCESS(201, "송금을 완료하였습니다."),
+
+    //==fail==//
     DEPOSIT_FAIL(400, "입금에 실패하였습니다."),
     DEPOSIT_RECORD_FAIL(400, "입금 거래내역 생성에 실패하였습니다."),
     REMIT_FAIL(400, "송금에 실패하였습니다."),
-    REMIT_RECORD_FAIL(400, "송금 거래내역 생성에 실패하였습니다."),
-    DEPOSIT_SUCCESS(201, "입금을 완료하였습니다."),
-    REMIT_SUCCESS(201, "송금을 완료하였습니다.");
+    REMIT_RECORD_FAIL(400, "송금 거래내역 생성에 실패하였습니다.");
 
     private final int status;
     private final String value;
