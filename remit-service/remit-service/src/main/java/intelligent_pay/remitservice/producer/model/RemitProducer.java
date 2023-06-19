2cbcc129cccc7b9548e1ace051aad232e0731320
liveforone <yc4852@gmail.com>
2023-06-19 16:34:08 +0900

    Add async.

--- remit-service/remit-service/src/main/java/intelligent_pay/remitservice/producer/model/RemitProducer.java
+++ remit-service/remit-service/src/main/java/intelligent_pay/remitservice/producer/model/RemitProducer.java
@@ -1,11 +1,13 @@
 package intelligent_pay.remitservice.producer.model;
 
 import com.google.gson.Gson;
+import intelligent_pay.remitservice.async.AsyncConstant;
 import intelligent_pay.remitservice.dto.record.RecordRequest;
 import intelligent_pay.remitservice.producer.log.ProducerLog;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import static intelligent_pay.remitservice.producer.model.ProducerTopic.*;
@@ -18,6 +20,7 @@ public class RemitProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
     Gson gson = new Gson();
 
+    @Async(AsyncConstant.commandAsync)
     public void depositRecord(RecordRequest recordRequest) {
         String jsonOrder = gson.toJson(recordRequest);
         String topic = DEPOSIT_RECORD;
@@ -25,6 +28,7 @@ public class RemitProducer {
         log.info(ProducerLog.KAFKA_SEND_LOG.getValue() + topic);
     }
 
+    @Async(AsyncConstant.commandAsync)
     public void withdrawRecord(RecordRequest recordRequest) {
         String jsonOrder = gson.toJson(recordRequest);
         String topic = WITHDRAW_RECORD;
